cdc3e62140b7ea9bec9350590ff5e35c1fc02977
Masahiro Fujioka <fujioka8700@gmail.com>
2022-04-29 16:34:57 +0900

    [update] <div> CSSボックス

--- resources/views/javascript/basic01.blade.php
+++ resources/views/javascript/basic01.blade.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First Web Page</title>
+    <style>
+        .box {
+            border: 1px solid #cccccc;
+            width: 300px;
+            height: 150px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    A good start is half successful
+    <div class="box">
+        Better Life
+    </div>
 </body>
 </html>
