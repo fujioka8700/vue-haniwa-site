e6d0b04975755feaa9732e2312cf0dcf8e07a389
Masahiro Fujioka <fujioka8700@gmail.com>
2022-04-29 20:30:01 +0900

    [change] padding追加

--- resources/views/javascript/basic01.blade.php
+++ resources/views/javascript/basic01.blade.php
@@ -3,48 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>absolute Box</title>
+    <title>Box</title>
     <style>
-        .redBox {
+        .box {
             border: 1px solid #cccccc;
-            width: 200px;
-            height: 200px;
-            text-align: center;
+            width: 300px;
             border-radius: 8px;
-            background-color: #ff0000;
-            position: relative;
-        }
-        .greenBox {
-            border: 1px solid #cccccc;
-            width: 200px;
-            height: 200px;
-            text-align: center;
-            border-radius: 8px;
-            background-color: #00ff00;
-            position: relative;
-            margin-top: 20px;
-        }
-        .blueBox {
-            border: 1px solid #cccccc;
-            width: 200px;
-            height: 200px;
-            text-align: center;
-            border-radius: 8px;
-            background-color: #0000ff;
-            position: relative;
-            margin-top: 20px;
+            padding: 15px 15px 15px 15px;
         }
     </style>
 </head>
 <body>
-    <div class="redBox">
-        
-    </div>
-    <div class="greenBox">
-        
-    </div>
-    <div class="blueBox">
-
+    <div class="box">
+        城の範囲は南北1.7km、東西0.8kmで南北に細長く、本丸は城の東側（現JR伊丹駅付近）に位置し、有岡城跡史跡公園となっている。明治時代の鉄道敷設のため、本丸東側の大半を失っている。防御の要所に3つの砦が配置され、北に「きしの砦」（現猪名野神社付近）、西に「上臈塚砦」（現墨染寺付近）、南に「鵯塚砦」が築かれていた｡ 
     </div>
 </body>
 </html>
