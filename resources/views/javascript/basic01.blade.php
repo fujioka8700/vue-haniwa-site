def273e9f5defbff9ce7800a3696daf41461e75f
Masahiro Fujioka <fujioka8700@gmail.com>
2022-04-30 06:47:32 +0900

    [change] 画像をfloat設定追加

--- resources/views/javascript/basic01.blade.php
+++ resources/views/javascript/basic01.blade.php
@@ -10,25 +10,20 @@
             width: 250px;
             border-radius: 8px;
             padding: 15px 15px 15px 15px;
+            font-size: 16px;
+            font-family: "Times New Roman", Times, serif;
         }
-        #imgBox {
-            width: 100%;
-        }
-        #textBox {
-            text-align: center;
-            width: 100%;
-            line-height: 30px
+        .box img {
+            width: 80px;
+            float: left;
         }
     </style>
 </head>
 <body>
     <div class="box">
-        <div id="imgBox">
-            <img src="{{ asset('images/animal_wallaby_kangaroo_s.png') }}" alt="ワラビー">
-        </div>
-        <div id="textBox">
-            あの有名なワラビーです。
-        </div>
+        <img src="{{ asset('images/animal_wallaby_kangaroo_s.png') }}" alt="ワラビー">
+        ワラビー (wallaby) は、フクロネズミ目（有袋類）カンガルー科に属する動物のうち、カンガルーやワラルーよりも小さな種に対し、一般的に使われる名称である。特に明確な定義付けはなされていないものの、約30種いる。カンガルーに比べ、後ろ足が小さく尾が短い。しかし、後ろ足で跳躍し移動すること、育児嚢で子供を育てることなど、基本的な習性はカンガルーと同じである。
+        小型の森林に生息するワラビーはヤブワラビー（ヤブワラビー属）、ドルコプシス（ドルコプシス属およびコドルコプシス属 (en)) として知られる。ワラビーの名はシドニー周辺で生活していたアボリジニのEora族の言葉に由来している。若いワラビーは他のカンガルー類と同様に"ジョーイ (joey)"として知られる。 
     </div>
 </body>
 </html>
