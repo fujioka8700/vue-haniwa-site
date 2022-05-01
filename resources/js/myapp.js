0c2e562764a5c86f140ca542d61f25e615fff027
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 18:49:01 +0900

    [change] 犬を探すゲーム完成

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,17 +1,35 @@
 // 犬を探すゲーム
 const maps = [
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,2,1,1],
-    [1,1,1,1]
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
 ];
 let mapHTML = "";
 
 for (let i = 0; i < maps.length; i++) {
     for (let j = 0; j < maps[i].length; j++) {
-        mapHTML += "<input type='button' value=''>"
+        maps[i][j] = Math.floor(Math.random() * 2) + 1;
+    }
+}
+
+for (let i = 0; i < maps.length; i++) {
+    for (let j = 0; j < maps[i].length; j++) {
+        mapHTML += "<input type='button' value='' onclick='doButtonClick(this, " + maps[i][j] + ")'>"
     }
     mapHTML += "<br>";
 }
 
 document.getElementById("result").innerHTML = mapHTML;
+
+function doButtonClick(obj, value) {
+    if (value == 1) {
+        obj.value = "*"; 
+    }
+
+    if (value == 2) {
+        obj.value = "Dog"; 
+    }
+}
+
+window.doButtonClick = doButtonClick;
