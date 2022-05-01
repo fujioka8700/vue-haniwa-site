9605c36d22d8bd949922c0268ea29a4bc52cb6c5
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 16:36:53 +0900

    [change] While ループステートメント

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,16 +1,6 @@
-// Switchステートメント
-const key = 555;
-switch (key) {
-    case 1:
-        document.write("Pay by Visa Card");
-        break;
-    case 2:
-        document.write("Pay by Master Card");
-        break;
-    case 3:
-        document.write("Pay by Paypal");
-        break;
-    default:
-        document.write("Pay by face to face");
-        break;
+// While ループステートメント
+let i = 0;
+while (i < 10) {
+    document.write(i + ", ");
+    i++;
 }
