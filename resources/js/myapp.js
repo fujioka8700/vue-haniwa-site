0e7f1619f8ff541de9f7f5a232ec4fe5f40ae847
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 10:47:43 +0900

    [change] JavaScriptは動的言語の説明

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,7 +1,13 @@
-let basket = "Apple";
-document.write(basket);
+let x;         // undefined
+let a = 9;     // number
+let b = 1.5;   // number
+let c = true;  // boolean
+let d = "abc"; // string
+let e = 'abc'; // string
 
-document.write("<br>");
-
-basket = "Orange";
-document.write(basket);
+document.write(typeof(x) + "<br>");
+document.write(typeof(a) + "<br>");
+document.write(typeof(b) + "<br>");
+document.write(typeof(c) + "<br>");
+document.write(typeof(d) + "<br>");
+document.write(typeof(e) + "<br>");
