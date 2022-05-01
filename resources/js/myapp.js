f324e7349e995203a23bb3bbee75a87204ff2911
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 16:27:46 +0900

    [change] 論理演算子

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,6 @@
-// If条件付きステートメント
-let age = 1;
+// 論理演算子
 
-if (age >= 18) {
-    document.write("18歳以上は成人です");
-} else if (age == 17) {
-    document.write("ギリギリセーフです");
-} else {
-    document.write("子供です");
-}
+document.write(true && false);
+document.write(true || false);
+document.write(true != false);
+document.write(!true);
