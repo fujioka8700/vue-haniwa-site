91237df141b400cf1b4d858cbe8bce1eb942afdb
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 16:09:18 +0900

    [change] Web電卓を作成する

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,7 +1,43 @@
-function doClick() {
-    let divObj = document.getElementById("username");
-    let value = divObj.value;
-    alert(value);
+function getValue() {
+    const aObj = document.getElementById("a");
+    const bObj = document.getElementById("b");
+    const a = parseInt(aObj.value);
+    const b = parseInt(bObj.value);
+    return {
+        a: a,
+        b: b,
+    }
 }
 
-window.doClick = doClick;
+function doAdd() {
+    let a = getValue().a;
+    let b = getValue().b;
+    let resultObj = document.getElementById("result");
+    resultObj.innerHTML = a + b;
+}
+
+function doSub() {
+    let a = getValue().a;
+    let b = getValue().b;
+    let resultObj = document.getElementById("result");
+    resultObj.innerHTML = a - b;
+}
+
+function doMultiply() {
+    let a = getValue().a;
+    let b = getValue().b;
+    let resultObj = document.getElementById("result");
+    resultObj.innerHTML = a * b;
+}
+
+function doDivide() {
+    let a = getValue().a;
+    let b = getValue().b;
+    let resultObj = document.getElementById("result");
+    resultObj.innerHTML = a / b;
+}
+
+window.doAdd = doAdd;
+window.doSub = doSub;
+window.doMultiply = doMultiply;
+window.doDivide = doDivide;
