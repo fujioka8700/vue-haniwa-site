e858a9eac5e8eef6697dd8c4ef4a17fd4eb0b7bd
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 16:46:28 +0900

    [change] 1次元配列

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,4 +1,10 @@
-// For ループステートメント
-for (let i = 0; i < 10; i++) {
-    document.write(i + ", ");
+// 1次元配列
+let scores = [90, 70, 50, 80, 60, 85];
+
+scores.forEach(element => {
+    document.write(element + "<br>");
+});
+
+for (let i = 0; i < scores.length; i++) {
+    document.write(scores[i] + "<br>");
 }
