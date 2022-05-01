8da85f081017703c92216f0a15a69ab4335586a1
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-01 16:32:57 +0900

    [change] Switchステートメント

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,6 +1,16 @@
-// 論理演算子
-
-document.write(true && false);
-document.write(true || false);
-document.write(true != false);
-document.write(!true);
+// Switchステートメント
+const key = 555;
+switch (key) {
+    case 1:
+        document.write("Pay by Visa Card");
+        break;
+    case 2:
+        document.write("Pay by Master Card");
+        break;
+    case 3:
+        document.write("Pay by Paypal");
+        break;
+    default:
+        document.write("Pay by face to face");
+        break;
+}
