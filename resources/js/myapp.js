adbd876ebc7cf20f14aa81043ed632f0b890e584
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-03 09:17:46 +0900

    [change] 都道府県と地名に変更

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,15 +1,36 @@
 // セカンダリリンケージドロップダウン
 
-const countryArray = [ "China", "United States" ];
+const prefecturesArray = [ "東京", "大阪" ];
+const citiesArray = [
+    [],
+    ["原宿", "渋谷"],
+    ["梅田", "心斎橋"]
+]
 
 window.onload = function() {
-    let countryObj = document.getElementById("country");
+    const prefecturesObj = document.getElementById("prefectures");
 
-    for (let i = 0; i < countryArray.length; i++) {
+    for (let i = 0; i < prefecturesArray.length; i++) {
         let option = document.createElement("option");
-        option.text = countryArray[i];
-        option.value = countryArray[i];
+        option.text = prefecturesArray[i];
+        option.value = prefecturesArray[i];
         
-        countryObj.options[countryObj.options.length] = option;   
+        prefecturesObj.options[prefecturesObj.options.length] = option;   
     }
+
+    function doPrefecturesChange(obj) {
+        let index = obj.selectedIndex;
+        let citiesObj = document.getElementById("cities");
+        citiesObj.options.length = 0;
+
+        for (let i = 0; i < prefecturesArray.length; i++) {
+            let option = document.createElement("option");
+            option.text = citiesArray[index][i];
+            option.value = citiesArray[index][i];
+            
+            citiesObj.options[citiesObj.options.length] = option;
+        }
+    }
+
+    window.doPrefecturesChange = doPrefecturesChange;
 }
--- resources/views/javascript/basic01.blade.php
+++ resources/views/javascript/basic01.blade.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-Control" content="no-cache">
     <title>JavaScriptの基本概念</title>
 </head>
 <body>
-    <select name="country" id="country">
-        <option value="">--Please Select Country--</option>
+    <select name="prefectures" id="prefectures" onchange="doPrefecturesChange(this)">
+        <option value="">--都道府県を選択--</option>
     </select>
+    <select name="cities" id="cities">
+        <option value="">--地名を選択--</option>
+    </select>
+
     <script src="{{ mix('js/myapp.js') }}"></script>
 </body>
 </html>
