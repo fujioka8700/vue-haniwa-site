59d8ccbb0b156d31b1fef91bef9ee09f21aa26ee
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-04 14:39:22 +0900

    [change] キャンバスの中心に円を描く

--- resources/views/javascript/basic01.blade.php
+++ resources/views/javascript/basic01.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Cache-Control" content="no-cache">
-    <title>DOM</title>
+    <title>キャンバス</title>
     <style>
         #myCanvas {
             border: 1px solid #c3c3c3;
@@ -13,7 +13,7 @@
     </style>
 </head>
 <body>
-    <canvas id="myCanvas" width="400" height="200"></canvas>
+    <canvas id="myCanvas" width="200" height="200"></canvas>
 
     <script src="{{ mix('js/myapp.js') }}"></script>
 </body>
