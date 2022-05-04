7f5f586f17dedce9c57d1e3c2339c575d9644e6a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-04 11:18:27 +0900

    [change] キャンバスに線を引く

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,22 @@
-// タイマー
-let doSetInterval = function() {
-   let dataDiv = document.getElementById("dateDiv");
+// canvas要素にプログレスバーをひく
+let canvas = document.getElementById("myCanvas");
 
-   // Date オブジェクトは、
-   // 単一の瞬間の時刻をプラットフォームに依存しない形式で表します。 
-   let d = new Date();
-   let dateString = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-   
-   dataDiv.innerHTML = dateString;
-}
+// キャンバスに描画するためのメソッドを提供するオブジェクトを返す
+let context = canvas.getContext("2d");
 
-// 一定の遅延間隔を置いて関数やコードスニペットを繰り返し呼び出します。
-setInterval(doSetInterval, 1000);
+// Canvasは「パス」と呼ばれるもので線や曲線、
+// 図形の外形を定義します（パスの概念はAdobe Illustratorのパスに似ています）。
+
+// 現在のパスをリセットする際に使用します。 
+context.beginPath();
+
+// moveTo(x, y)メソッドは、新しいサブパスの開始点を座標指定する際に使用します。
+// 指定する座標は、<canvas>要素の左上端からの距離となります。 
+context.moveTo(20, 100);
+
+// lineTo(x, y)メソッドは、直前の座標と指定座標を結ぶ直線を引く際に使用します。
+// 指定する座標は、<canvas>要素の左上端からの距離となります。
+context.lineTo(200,100);
+
+// 現在の線スタイルでサブパスを輪郭表示する際に使用します。
+context.stroke();
--- resources/views/javascript/basic01.blade.php
+++ resources/views/javascript/basic01.blade.php
@@ -7,11 +7,15 @@
     <meta http-equiv="Cache-Control" content="no-cache">
     <title>DOM</title>
     <style>
-        
+        #myCanvas {
+            border: 1px solid #c3c3c3;
+            width: 400px;
+            height: 200px;
+        }
     </style>
 </head>
 <body>
-    <div id="dateDiv"></div>
+    <canvas id="myCanvas" width="400" height="200"></canvas>
 
     <script src="{{ mix('js/myapp.js') }}"></script>
 </body>
