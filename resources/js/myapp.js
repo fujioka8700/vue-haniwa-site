9042cce6fc24e337b01530d35537b489720a653e
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-05 09:08:38 +0900

    [clean] 円を描く際の補足を追加

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -4,5 +4,12 @@ let context = canvas.getContext("2d");
 
 context.fillStyle = "#0000ff";
 context.beginPath();
+
+// サブパスに円弧を加えます。
+// void ctx.arc(x, y, radius, startAngle, endAngle [, counterclockwise]);
+// angle 意味：角度
 context.arc(100, 100, 50, 0, 2 * Math.PI);
+
+// fill()メソッドは、現在の塗りつぶしスタイルでサブパスを塗りつぶす際に使用します。
+// 塗りつぶしスタイルは、fillStyleなどの属性で指定します。
 context.fill();
