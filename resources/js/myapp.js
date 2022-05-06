deea8f7fba34ebfdcca48e4f6394acad7a10fa85
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 08:34:42 +0900

    [change] オブジェクトのdeep copyについて

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,17 +1,32 @@
 "use strict";
 
+// 引数の`obj`を浅く複製したオブジェクトを返す
 const shallowClone = (obj) => {
+    // console.log(obj);
+    // return;
     return Object.assign({}, obj);
 };
+// 引数の`obj`を深く複製したオブジェクトを返す
+function deepClone(obj) {
+    const newObj = shallowClone(obj);
+    // console.log(newObj);
+    // プロパティがオブジェクト型であるなら、再帰的に複製する
+    Object.keys(newObj)
+        .filter(k => typeof newObj[k] === "object")
+        .forEach(function(k) {
+            // console.log(newObj[k]);
+            // return;
+            newObj[k] = deepClone(newObj[k]);
+        });
+    console.log(newObj);
+    return newObj;
+}
 const obj = {
     level: 1,
     nest: {
         level: 2
-    },
+    }
 };
-const cloneObj = shallowClone(obj);
-// `nest`プロパティのオブジェクトは同じオブジェクトのままになる 
-console.log(cloneObj.nest === obj.nest); // => true
-console.log(cloneObj === obj); // => true
-console.log(cloneObj);
-console.log(obj);
+const cloneObj = deepClone(obj);
+// `nest`オブジェクトも再帰的に複製されている
+console.log(cloneObj.nest === obj.nest); // => false
