f87244391907a6c2953c80b85bbb7e41c0bb4ec6
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-06 19:13:23 +0900

    [change] for ofについて

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,31 +1,8 @@
 "use strict";
 
 const array = [1, 2, 3];
-array.some(function(value){
+for (const value of array) {
     console.log(value);
-    return true;
-});
-
-array.forEach(function(value){
-    console.log(value);
-    return true;
-});
-
-const isEven = num => num % 2 === 0;
-
-const array2 = [1, 5, 10, 15, 20];
-console.log(array2.filter(isEven)); // => [10, 20]
-
-const obj = {
-    "a": 1,
-    "b": 2,
-    "c": 3
-};
-// 注記: ループのたびに毎回新しいブロックに変数keyが定義されるため、再定義エラーが発生しない
-for (const key in obj) {
-    const value = obj[key];
-    console.log(`key:${key}, value:${value}`);
 }
-// "key:a, value:1"
-// "key:b, value:2"
-// "key:c, value:3"
+
+console.log(typeof array);
