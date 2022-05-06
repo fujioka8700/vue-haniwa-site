9e7cf279eb97e92bb5ee89d8510eb3fe0099ffcc
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-06 16:34:40 +0900

    [change] 暗黙的な型変換～明示的な型変換の途中

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,26 +1,22 @@
 "use strict";
 
-// 分割代入 
-const obj = {
-    key  : "value",
-    key2 : "value2"
-};
+console.log(1 === "1");
+console.log(1 == "1");
 
-const { key } = obj;
-console.log(key);
+console.log(1 + true);
+console.log(true + true);
 
-console.log(null ?? "右辺の値");
-console.log(undefined ?? "右辺の値");
-console.log(true ?? "左辺の値");
-console.log(false ?? "右辺の値");
-console.log(0 ?? "右辺の値");
-console.log("文字列" ?? "右辺の値");
+console.log(0 == false);
+console.log(10 == ["10"]);
 
-const valueA = false ? "A" : "B";
-console.log(valueA);
+console.log(1 === "1");
+console.log(0 === false);
+console.log(10 == ["10"]);
 
-console.log(1+2*3);
-console.log((1+2)*3);
+console.log(typeof(1 + "2"));
 
-const a = 1, b = 2, c = a + b;
-console.log(c);
+console.log(1 - "2");
+console.log(1 - 2);
+
+let isOK = Boolean(null);
+console.log(isOK);
