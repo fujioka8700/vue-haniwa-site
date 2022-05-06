4d9334842bb4752b35b1cf91cae6e959bf727bf8
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-06 17:08:55 +0900

    [change] NaNについて

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,37 +1,24 @@
 "use strict";
 
-let x;
+let x = "文字列" + x;
+console.log(x);
 
-if (!x) {
-    console.log('falsyな値なら表示:' + x);
+// let y = "文字列" + Symbol("シンボルの説明");
+// console.log(y);
+
+let y = "文字列" + String(Symbol("シンボルの説明"));
+console.log(y);
+
+// const input = window.prompt("数字を入力してください", "42");
+
+const userInput = "任意の文字列";
+const num = Number.parseInt(userInput, 10);
+console.log(num);
+if (Number.isNaN(num)) {
+    console.log("パースした結果NaNになった", num);
 }
 
-let x2;
+let n = Number({});
+console.log(typeof n);
 
-if (x2 == undefined) {
-    console.log('xがundefinedなら表示:' + x);
-}
-
-let x3 = String([1, 2, 3]);
-console.log(x3);
-
-let x4 = String({key:"value"});
-console.log(x4);
-
-const sym = Symbol();
-console.log(sym); //[object Symbol] { ... }
-console.log(typeof sym); //symbol
-
-const sym1 = Symbol();
-const sym2 = Symbol();
-console.log(sym1);
-console.log(sym2);
-
-console.log(sym1 === sym2); //false
-
-const sym3 = [];
-for (let i = 0; i < 100; i++) {
-    sym3.push(Symbol());
-}
-
-sym3.forEach(element => console.log(element));
+console.log(isNaN(NaN));
