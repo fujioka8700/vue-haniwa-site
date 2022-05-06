317211820c0607bd146a4d2a034525b18ebb15c0
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-06 17:28:37 +0900

    [change] NaNはNot a NumberだけどNumber型

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,24 +1,11 @@
 "use strict";
 
-let x = "文字列" + x;
-console.log(x);
-
-// let y = "文字列" + Symbol("シンボルの説明");
-// console.log(y);
-
-let y = "文字列" + String(Symbol("シンボルの説明"));
-console.log(y);
-
-// const input = window.prompt("数字を入力してください", "42");
-
-const userInput = "任意の文字列";
-const num = Number.parseInt(userInput, 10);
-console.log(num);
-if (Number.isNaN(num)) {
-    console.log("パースした結果NaNになった", num);
+function sum(...values) {
+    return values.reduce((total, value) => {
+        // `value`をNumberで明示的に数値へ変換してから加算する
+        return total + Number(value);
+    }, 0);
 }
-
-let n = Number({});
-console.log(typeof n);
-
-console.log(isNaN(NaN));
+const x = 1, z = 10;
+let y; // `y`はundefined
+console.log(sum(x, y, z)); // => NaN
