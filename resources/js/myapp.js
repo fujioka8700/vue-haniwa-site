7d2ef79a5f2106384a77c208072a5b061864c0d1
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-06 17:30:17 +0900

    [change] 明示的な変換でも解決しないこと

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,11 +1,13 @@
 "use strict";
 
-function sum(...values) {
-    return values.reduce((total, value) => {
-        // `value`をNumberで明示的に数値へ変換してから加算する
-        return total + Number(value);
-    }, 0);
+// 空文字列かどうかを判定
+function isEmptyString(str) {
+    // `str`がfalsyな値なら、`isEmptyString`関数は`true`を返す
+    return !Boolean(str);
 }
-const x = 1, z = 10;
-let y; // `y`はundefined
-console.log(sum(x, y, z)); // => NaN
+// 空文字列列の場合は、trueを返す
+console.log(isEmptyString("")); // => true
+// falsyな値の場合は、trueを返す
+console.log(isEmptyString(0)); // => true
+// undefinedの場合は、trueを返す
+console.log(isEmptyString()); // => true
