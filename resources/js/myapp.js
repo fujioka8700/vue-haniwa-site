74283acb95b0242dadfdc5a672a4649212a25130
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 06:37:44 +0900

    [change] toStringメソッド

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,30 +1,14 @@
 "use strict";
 
-function printWidgetTitle(widget) {
-    const title = widget?.window?.title ?? "未定義";
-    console.log(`ウィジェットのタイトルは${title}です`);
-}
-printWidgetTitle({
-    window: {
-        title: "Book Viewer"
-    }
-}); // "ウィジェットのタイトルはBook Viewerです" と出力される
-printWidgetTitle({
-    // タイトルが定義されてない空のオブジェクト
-}); // "ウィジェットのタイトルは未定義です" と出力される
+const obj = { key: "value" };
+console.log(obj.toString()); // => "[object Object]"
+// `String`コンストラクタ関数は`toString`メソッドを呼んでいる
+console.log(String(obj)); // => "[object Object]"
 
-const languages = {
-    ja: {
-        hello: "こんにちは！"
-    },
-    en: {
-        hello: "Hello!"
+// 独自のtoStringメソッドを定義
+const customObject = {
+    toString() {
+        return "custom value";
     }
 };
-const langJapanese = "ja";
-const langKorean = "ko";
-const messageKey = "hello";
-// Optional chaining演算子（`?.`）とブラケット記法を組みわせた書き方
-console.log(languages?.[langJapanese]?.[messageKey]); // => "こんにちは！"
-// `languages`に`ko`プロパティが定義されていないため、`undefined`を返す
-console.log(languages?.[langKorean]?.[messageKey]); // => undefined
+console.log(String(customObject)); // => "custom value"
