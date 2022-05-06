f25d3bf13e870f3c9b1f97f8b04269162c62e629
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 07:58:47 +0900

    [change] オブジェクトのspread構文でのマージ

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -2,20 +2,19 @@
 
 const objectA = { a: "a" };
 const objectB = { b: "b" };
-const merged = Object.assign({}, objectA, objectB);
+const merged = {
+    ...objectA,
+    ...objectB
+};
 console.log(merged); // => { a: "a", b: "b" }
 
-const objecta = { a: "a" };
-const objectb = { b: "b" };
-const merged2 = Object.assign(objectA, objectB);
-console.log(merged2); // => { a: "a", b: "b" }
-// `objectA`が変更されている
-console.log(objecta); // => { a: "a", b: "b" }
-console.log(merged2 === objectb); // => true
-
 // `version`のプロパティ名が被っている
-const objectA2 = { version: "a" };
-const objectB2 = { version: "b" };
-const merged3 = Object.assign({}, objectA2, objectB2);
+const objectAA = { version: "a" };
+const objectBB = { version: "b" };
+const merged1 = {
+    ...objectAA,
+    ...objectBB,
+    other: "other"
+};
 // 後ろにある`objectB`のプロパティで上書きされる
-console.log(merged3); // => { version: "b" }
+console.log(merged1); // => { version: "b", other: "other" }
