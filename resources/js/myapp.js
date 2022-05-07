fd34337a452cd28a39d34d3c96e43b49f5346d7d
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 17:55:50 +0900

    [change] 配列同士を連結

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,15 +1,9 @@
 "use strict";
 
 const array = ["A", "B", "C"];
-array.push("D"); // "D"を末尾に追加
-console.log(array); // => ["A", "B", "C", "D"]
-const poppedItem = array.pop(); // 最末尾の要素を削除し、その要素を返す
-console.log(poppedItem); // => "D"
-console.log(array); // => ["A", "B", "C"]
+const newArray = array.concat(["D", "E"]);
+console.log(newArray); // => ["A", "B", "C", "D", "E"]
 
 const array2 = ["A", "B", "C"];
-array2.unshift("S"); // "S"を先頭に追加
-console.log(array2); // => ["S", "A", "B", "C"]
-const shiftedItem = array2.shift(); // 先頭の要素を削除
-console.log(shiftedItem); // => "S"
-console.log(array2); // => ["A", "B", "C"]
+const newArray2 = array2.concat("新しい要素");
+console.log(newArray2); // => ["A", "B", "C", "新しい要素"]
