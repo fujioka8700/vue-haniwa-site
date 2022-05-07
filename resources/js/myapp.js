4e546772992661098b95d85c062972aa5ad57d2d
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 06:57:13 +0900

    [change] 文字列で真偽値の取得

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,28 +1,34 @@
 "use strict";
 
-const str = "ABCDE";
-console.log(str.slice(1)); // => "BCDE"
-console.log(str.slice(1, 5)); // => "BCDE"
-// マイナスを指定すると後ろからの位置となる
-console.log(str.slice(-1)); // => "E"
-// インデックスが1から4の範囲を取り出す
-console.log(str.slice(1, 4)); // => "BCD"
-// 第一引数 > 第二引数の場合、常に空文字列を返す
-console.log(str.slice(4, 1)); // => ""
+// 検索対象となる文字列
+const str = "にわにはにわにわとりがいる";
+// indexOfは先頭から検索しインデックスを返す - "**にわ**にはにわにわとりがいる"
+// "にわ"の先頭のインデックスを返すため 0 となる
+console.log(str.indexOf("にわ")); // => 0
+// lastIndexOfは末尾から検索しインデックスを返す- "にわにはにわ**にわ**とりがいる"
+console.log(str.lastIndexOf("にわ")); // => 6
+// 指定した文字列が見つからない場合は -1 を返す
+console.log(str.indexOf("未知のキーワード")); // => -1
 
-const str2 = "ABCDE";
-console.log(str2.substring(1)); // => "BCDE"
-console.log(str2.substring(1, 5)); // => "BCDE"
-// マイナスを指定すると0として扱われる
-console.log(str2.substring(-1)); // => "ABCDE"
-// 位置:1から4の範囲を取り出す
-console.log(str2.substring(1, 4)); // => "BCD"
-// 第一引数 > 第二引数の場合、引数が入れ替わる
-// str.substring(1, 4)と同じ結果になる
-console.log(str2.substring(4, 1)); // => "BCD"
 
-const url = "https://example.com?param=1";
-const indexOfQuery = url.indexOf("?");
-console.log(indexOfQuery);
-const queryString = url.slice(indexOfQuery);
-console.log(queryString); // => "?param=1"
+const str2 = "JavaScript";
+const searchWord = "Script";
+const index = str2.indexOf(searchWord);
+if (index !== -1) {
+    console.log(`${searchWord}が見つかりました`);
+} else {
+    console.log(`${searchWord}は見つかりませんでした`);
+}
+
+
+// 検索対象となる文字列
+const str3 = "にわにはにわにわとりがいる";
+// startsWith - 検索文字列が先頭ならtrue
+console.log(str3.startsWith("にわ")); // => true
+console.log(str3.startsWith("いる")); // => false
+// endsWith - 検索文字列が末尾ならtrue
+console.log(str3.endsWith("にわ")); // => false
+console.log(str3.endsWith("いる")); // => true
+// includes - 検索文字列が含まれるならtrue
+console.log(str3.includes("にわ")); // => true
+console.log(str3.includes("いる")); // => true
