6321d313a4feec88da2fe5546318de41c537216c
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 18:00:22 +0900

    [change] 配列同士の展開

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const array = ["A", "B", "C"];
-const newArray = array.concat(["D", "E"]);
-console.log(newArray); // => ["A", "B", "C", "D", "E"]
+// Spread構文を使った場合
+const newArray = ["X", "Y", "Z", ...array];
+// concatメソッドの場合
+const newArrayConcat = ["X", "Y", "Z"].concat(array);
+console.log(newArray); // => ["X", "Y", "Z", "A", "B", "C"]
+console.log(newArrayConcat); // => ["X", "Y", "Z", "A", "B", "C"]
 
 const array2 = ["A", "B", "C"];
-const newArray2 = array2.concat("新しい要素");
-console.log(newArray2); // => ["A", "B", "C", "新しい要素"]
+const newArray2 = ["X", ...array2, "Z"];
+console.log(newArray2); // => ["X", "A", "B", "C", "Z"]
