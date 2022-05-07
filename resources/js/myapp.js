4b8f035f6644f5064c15feae7bbb9dc61dcb5414
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 17:42:02 +0900

    [change] 追加と削除

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,32 +1,15 @@
 "use strict";
 
-const array = ["A", "B", "C", "D", "E"];
-// インデックス1から4の範囲を取り出す
-console.log(array.slice(1, 4)); // => ["B", "C", "D"]
-// 第二引数を省略した場合は、第一引数から末尾の要素までを取り出す
-console.log(array.slice(1)); // => ["B", "C", "D", "E"]
-// マイナスを指定すると後ろからの数えた位置となる
-console.log(array.slice(-1)); // => ["E"]
-// 第一引数と第二引数が同じ場合は、空の配列を返す
-console.log(array.slice(1, 1)); // => []
-// 第一引数 > 第二引数の場合、常に空配列を返す
-console.log(array.slice(4, 1)); // => []
+const array = ["A", "B", "C"];
+array.push("D"); // "D"を末尾に追加
+console.log(array); // => ["A", "B", "C", "D"]
+const poppedItem = array.pop(); // 最末尾の要素を削除し、その要素を返す
+console.log(poppedItem); // => "D"
+console.log(array); // => ["A", "B", "C"]
 
-const array2 = ["Java", "JavaScript", "Ruby"];
-// `includes`は含まれているなら`true`を返す
-if (array2.includes("JavaScript")) {
-    console.log("配列にJavaScriptが含まれている");
-}
-
-// colorプロパティを持つオブジェクトの配列
-const colors = [
-    { "color": "red" },
-    { "color": "green" },
-    { "color": "blue" }
-];
-// `color`プロパティが"blue"のオブジェクトがあるかどうか
-const isIncludedBlueColor = colors.some((obj) => {
-    console.log("Hello");
-    return obj.color === "blue";
-});
-console.log(isIncludedBlueColor); // => true
+const array2 = ["A", "B", "C"];
+array2.unshift("S"); // "S"を先頭に追加
+console.log(array2); // => ["S", "A", "B", "C"]
+const shiftedItem = array2.shift(); // 先頭の要素を削除
+console.log(shiftedItem); // => "S"
+console.log(array2); // => ["A", "B", "C"]
