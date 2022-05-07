4be6c38f3993fbba6b92d8ea2011e6658d758e0f
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 10:36:06 +0900

    [change] インデックスを取得

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,38 +1,32 @@
 "use strict";
 
+const array = ["Java", "JavaScript", "Ruby"];
+const indexOfJS = array.indexOf("JavaScript");
 
-// 未定義の箇所が1つ含まれる疎な配列
-// インデックスが1の値を省略しているので、カンマが2つ続いていることに注意
-const sparseArray = [1,, 3];
-console.log(sparseArray.length); // => 3
-// 1番目の要素は存在しないため undefined が返る
-console.log(sparseArray[1]); // => undefined
+console.log(indexOfJS); // => 1
+console.log(array[indexOfJS]); // => "JavaScript"
 
+// "JS" という要素はないため `-1` が返される
+console.log(array.indexOf("JS")); // => -1
 
-const array = ["a", "b", "c"];
-//
-console.log(array.at(0)); // => "a"
-console.log(array.at(1)); // => "b"
-
-// 後ろから1つ目の要素にアクセス
-console.log(array.at(-1)); // => "c"
-
-// -1は、次のように書いた場合と同じ結果
-console.log(array[array.length - 1]); // => "c"
+const obj = { key: "value" };
+const array2 = ["A", "B", obj];
+console.log(array2.indexOf({ key: "value" })); // => -1
+// リテラルは新しいオブジェクトを作るため、異なるオブジェクトだと判定される
+console.log(obj === { key: "value" }); // => false
+// 等価のオブジェクトを検索してインデックスを返す
+console.log(array2.indexOf(obj)); // => 2
 
 
-const obj = {};
-const array2 = [];
-console.log(Array.isArray(obj)); // => false
-console.log(Array.isArray(array2)); // => true
-
-
-// TypedArrayを作成
-const typedArray = new Int8Array(8);
-console.log(Array.isArray(typedArray)); // => false
-
-const array3 = ["one", "two", "three"];
-const [first, second, third] = array3;
-console.log(first);  // => "one"
-console.log(second); // => "two"
-console.log(third);  // => "three"
+// colorプロパティを持つオブジェクトの配列
+const colors = [
+    { "color": "red" },
+    { "color": "green" },
+    { "color": "blue" }
+];
+// `color`プロパティが"blue"のオブジェクトのインデックスを取得
+const indexOfBlue = colors.findIndex((obj) => {
+    return obj.color === "green";
+});
+console.log(indexOfBlue); // => 2
+console.log(colors[indexOfBlue]); // => { "color": "blue" }
