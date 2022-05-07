c36b628be346a0b1b8d6cc9d549ae9fa0975d189
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 18:54:56 +0900

    [change] reduceメソッド

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,19 +1,11 @@
 "use strict";
 
 const array = [1, 2, 3];
-array.forEach((currentValue, index, array) => {
-    console.log(currentValue, index, array);
-});
-// コンソールの出力
-// 1, 0, [1, 2, 3]
-// 2, 1, [1, 2, 3]
-// 3, 2, [1, 2, 3]
-
-const array2 = [1, 2, 3];
-// 奇数の値を持つ要素だけを集めた配列を返す
-const newArray = array2.filter((currentValue, index, array) => {
-    return currentValue % 2 === 1;
-});
-console.log(newArray); // => [1, 3]
-// 元の配列とは異なるインスタンス
-console.log(array2 === newArray); // => false
+// すべての要素を加算した値を返す
+// accumulatorの初期値は`0`
+const totalValue = array.reduce((accumulator, currentValue, index, array) => {
+    console.log(accumulator);
+    return accumulator + currentValue;
+}, 0);
+// 0 + 1 + 2 + 3という式の結果が返り値になる
+console.log(totalValue); // => 6
