220ef89e2a618f5b0ebc0cdb6dd64e74aaef39c3
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 10:19:04 +0900

    [change] 配列と分割代入

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,22 +1,38 @@
 "use strict";
 
-// 親がnull、つまり親がいないオブジェクトを作る
-const obj = Object.create(null);
 
-// Object.prototypeを継承しないため、hasOwnPropertyが存在しない
-console.log(obj.hasOwnProperty); // => undefined
+// 未定義の箇所が1つ含まれる疎な配列
+// インデックスが1の値を省略しているので、カンマが2つ続いていることに注意
+const sparseArray = [1,, 3];
+console.log(sparseArray.length); // => 3
+// 1番目の要素は存在しないため undefined が返る
+console.log(sparseArray[1]); // => undefined
 
-// 空オブジェクトを作成
-const obj2 = {};
-// "toString"という値を定義してないのに、"toString"が存在している
-console.log(obj2["toString"]);// Function
-// Mapのような空オブジェクト
-const mapLike = Object.create(null);
-// toStringキーは存在しない
-console.log(mapLike["toString"]); // => undefined
 
-// Mapのような空オブジェクト
-const mapLike2 = Object.create(null);
-// `Object.prototype`を継承していないため呼び出すと例外が発生する
-// console.log(mapLike2.hasOwnProperty("key")); // => Error: hasOwnPropertyメソッドは呼び出せない
-console.log(Object.hasOwn(mapLike2, "key"));
+const array = ["a", "b", "c"];
+//
+console.log(array.at(0)); // => "a"
+console.log(array.at(1)); // => "b"
+
+// 後ろから1つ目の要素にアクセス
+console.log(array.at(-1)); // => "c"
+
+// -1は、次のように書いた場合と同じ結果
+console.log(array[array.length - 1]); // => "c"
+
+
+const obj = {};
+const array2 = [];
+console.log(Array.isArray(obj)); // => false
+console.log(Array.isArray(array2)); // => true
+
+
+// TypedArrayを作成
+const typedArray = new Int8Array(8);
+console.log(Array.isArray(typedArray)); // => false
+
+const array3 = ["one", "two", "three"];
+const [first, second, third] = array3;
+console.log(first);  // => "one"
+console.log(second); // => "two"
+console.log(third);  // => "three"
