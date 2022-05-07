0956ab41a967968ce60bfc83a1bc038420835153
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 18:49:03 +0900

    [change] filterメソッド

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,18 +1,19 @@
 "use strict";
 
-const myArray = ["A", "B", "C"];
-const result = myArray.push("D");
-// `push`の返り値は配列ではなく、追加後の配列のlength
-console.log(result); // => 4
-// `myArray`が参照する配列そのものが変更されている
-console.log(myArray); // => ["A", "B", "C", "D"]
+const array = [1, 2, 3];
+array.forEach((currentValue, index, array) => {
+    console.log(currentValue, index, array);
+});
+// コンソールの出力
+// 1, 0, [1, 2, 3]
+// 2, 1, [1, 2, 3]
+// 3, 2, [1, 2, 3]
 
-
-const myArrayB = ["A", "B", "C"];
-// `concat`の返り値は結合済みの新しい配列
-const newArray = myArrayB.concat("D");
-console.log(newArray); // => ["A", "B", "C", "D"]
-// `myArrayB`は変更されていない
-console.log(myArrayB); // => ["A", "B", "C"]
-// `newArray`と`myArrayB`は異なる配列オブジェクト
-console.log(myArrayB === newArray); // => false
+const array2 = [1, 2, 3];
+// 奇数の値を持つ要素だけを集めた配列を返す
+const newArray = array2.filter((currentValue, index, array) => {
+    return currentValue % 2 === 1;
+});
+console.log(newArray); // => [1, 3]
+// 元の配列とは異なるインスタンス
+console.log(array2 === newArray); // => false
