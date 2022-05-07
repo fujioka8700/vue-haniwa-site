074c3296e098bc5043767c42566a8d8df09f89de
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 17:38:35 +0900

    [change] 真偽値を取得

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,18 +1,32 @@
 "use strict";
 
+const array = ["A", "B", "C", "D", "E"];
+// インデックス1から4の範囲を取り出す
+console.log(array.slice(1, 4)); // => ["B", "C", "D"]
+// 第二引数を省略した場合は、第一引数から末尾の要素までを取り出す
+console.log(array.slice(1)); // => ["B", "C", "D", "E"]
+// マイナスを指定すると後ろからの数えた位置となる
+console.log(array.slice(-1)); // => ["E"]
+// 第一引数と第二引数が同じ場合は、空の配列を返す
+console.log(array.slice(1, 1)); // => []
+// 第一引数 > 第二引数の場合、常に空配列を返す
+console.log(array.slice(4, 1)); // => []
+
+const array2 = ["Java", "JavaScript", "Ruby"];
+// `includes`は含まれているなら`true`を返す
+if (array2.includes("JavaScript")) {
+    console.log("配列にJavaScriptが含まれている");
+}
+
 // colorプロパティを持つオブジェクトの配列
 const colors = [
     { "color": "red" },
     { "color": "green" },
     { "color": "blue" }
 ];
-// `color`プロパティが"blue"のオブジェクトを取得
-const blueColor = colors.find((obj) => {
+// `color`プロパティが"blue"のオブジェクトがあるかどうか
+const isIncludedBlueColor = colors.some((obj) => {
+    console.log("Hello");
     return obj.color === "blue";
 });
-console.log(blueColor); // => { "color": "blue" }
-// 該当する要素がない場合は`undefined`を返す
-const whiteColor = colors.find((obj) => {
-    return obj.color === "white";
-});
-console.log(whiteColor); // => undefined
+console.log(isIncludedBlueColor); // => true
