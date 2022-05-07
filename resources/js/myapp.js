dbef456eb37d73c0f297102994a809e2103f8fbb
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 18:20:57 +0900

    [change] lengthプロパティへの代入

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,11 +1,5 @@
 "use strict";
 
-const array = ["a", "b", "c"];
-// 1番目から1つの要素("b")を削除
-array.splice(1, 1);
-console.log(array); // => ["a", "c"]
-console.log(array.length); // => 2
-console.log(array[1]); // => "c"
-// すべて削除
-array.splice(0, array.length);
-console.log(array.length); // => 0
+const array = [1, 2, 3];
+array.length = 0; // 配列を空にする
+console.log(array); // => []
