8326ef69c69bebeaa51d3905d0f65e40e0ca7cc2
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 07:53:18 +0900

    [change] マッチした文字列の取得 matchAll

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,27 +1,14 @@
 "use strict";
 
-const str = "AB C D あいう DE えお";
-const alphabetsPattern = /[a-zA-Z]+/;
-// gフラグなしでは、最初の結果のみを含んだ特殊な配列を返す
-const results = str.match(alphabetsPattern);
-console.log(results);
-console.log(results.length); // => 1
-// マッチした文字列はインデックスでアクセスできる
-console.log(results[0]); // => "ABC"
-// マッチした文字列の先頭のインデックス
-console.log(results.index); // => 0
-// 検索対象となった文字列全体
-console.log(results.input); // => "ABC あいう DE えお"
-
-
-const str2 = "ABC あいう DE えお";
-const alphabetsPattern2 = /[a-zA-Z]+/g;
-// gフラグありでは、すべての検索結果を含む配列を返す
-const resultsWithG = str2.match(alphabetsPattern2);
-console.log(resultsWithG);
-console.log(resultsWithG.length); // => 2
-console.log(resultsWithG[0]); // => "ABC"
-console.log(resultsWithG[1]); // => "DE"
-// indexとinputはgフラグありの場合は追加されない
-console.log(resultsWithG.index); // => undefined
-console.log(resultsWithG.input); // => undefined
+const str = "ABC あいう DE えお";
+const alphabetsPattern = /[a-zA-Z]+/g;
+// matchAllはIteratorを返す
+const matchesIterator = str.matchAll(alphabetsPattern);
+console.log(matchesIterator);
+for (const match of matchesIterator) {
+    // マッチした要素ごとの情報を含んでいる
+    console.log(`match: "${match[0]}", index: ${match.index}, input: "${match.input}"`);
+}
+// 次の順番でコンソールに出力される
+// match: "ABC", index: 0, input: "ABC あいう DE えお"
+// match: "DE", index: 8, input: "ABC あいう DE えお"
