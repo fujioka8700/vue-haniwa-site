ff6727034c5cb0a21c2368470086a9226f69cf4e
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-07 18:28:49 +0900

    [change] 空の配列を代入

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,5 +1,13 @@
 "use strict";
 
-const array = [1, 2, 3];
-array.length = 0; // 配列を空にする
-console.log(array); // => []
+let array = [1, 2, 3];
+console.log(array.length); // => 3
+// 新しい配列で変数を上書き
+array = [];
+console.log(array.length); // => 0
+
+
+const array2 = [1, 2, 3];
+console.log(array2.length); // => 3
+// `const`で宣言された変数には再代入できない
+array2 = [];
