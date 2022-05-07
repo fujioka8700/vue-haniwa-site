ac47c61ddf950e521a48c5274f0b504f0ae214fe
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 07:57:39 +0900

    [change] マッチした文字列の一部を取得

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,15 @@
 "use strict";
 
-const str = "ABC あいう DE えお";
-const alphabetsPattern = /[a-zA-Z]+/g;
-// matchAllはIteratorを返す
-const matchesIterator = str.matchAll(alphabetsPattern);
-console.log(matchesIterator);
+// "ES(数字+)"にマッチするが、欲しい文字列は数字の部分のみ
+const pattern = /ES(\d+)/g;
+// iteratorを返す
+const matchesIterator = "ES2015、ES2016、ES2017".matchAll(pattern);
 for (const match of matchesIterator) {
     // マッチした要素ごとの情報を含んでいる
-    console.log(`match: "${match[0]}", index: ${match.index}, input: "${match.input}"`);
+    // 0番目はマッチした文字列全体、1番目がキャプチャの1番目である数字
+    console.log(`match: "${match[0]}", capture1: ${match[1]}, index: ${match.index}, input: "${match.input}"`);
 }
 // 次の順番でコンソールに出力される
-// match: "ABC", index: 0, input: "ABC あいう DE えお"
-// match: "DE", index: 8, input: "ABC あいう DE えお"
+// match: "ES2015", capture1: 2015, index: 0, input: "ES2015、ES2016、ES2017"
+// match: "ES2016", capture1: 2016, index: 7, input: "ES2015、ES2016、ES2017"
+// match: "ES2017", capture1: 2017, index: 14, input: "ES2015、ES2016、ES2017"
