2f8c7e964351cd4a4d0622226e896dd955421bd1
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 11:22:09 +0900

    [change] 変数を参照できる範囲を小さくする

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,39 +1,27 @@
 "use strict"
 
-{
-    // OUTERブロックスコープ
-    const x = "outer";
-    {
-        // INNERブロックスコープ
-        const x = "inner";
-        // 現在のスコープ(INNERブロックスコープ)にある`x`を参照する
-        console.log(x); // => "inner"
+function doHeavyTask() {
+    for (let i = 0; i < 1000; i++) {
+        console.log("Hello");        
     }
-    // 現在のスコープ(OUTERブロックスコープ)にある`x`を参照する
-    console.log(x); // => "outer"
 }
+const startTime = Date.now();
+doHeavyTask();
+const endTime = Date.now();
+console.log(`実行時間は${endTime - startTime}ミリ秒`);
 
-
-// グローバル変数はどのスコープからも参照できる
-const globalVariable = "グローバル";
-// ブロックスコープ
-{
-    // ブロックスコープ内には該当する変数が定義されてない -> 外側のスコープへ
-    console.log(globalVariable); // => "グローバル"
+// 実行時間を計測したい関数をコールバック関数として引数に渡す
+const measureTask2 = (taskFn) => {
+    const startTime = Date.now();
+    taskFn();
+    const endTime = Date.now();
+    console.log(`実行時間は${endTime - startTime}ミリ秒`);
+};
+function doHeavyTask2() {
+    for (let i = 0; i < 1000; i++) {
+        console.log("Hello");        
+    }
 }
-// 関数スコープ
-function fn() {
-    // 関数ブロックスコープ内には該当する変数が定義されてない -> 外側のスコープへ
-    console.log(globalVariable); // => "グローバル"
-}
-fn();
+measureTask2(doHeavyTask2);
 
-// ビルトインオブジェクトは実行環境が自動的に定義している
-// どこのスコープから参照してもReferenceErrorにはならない
-console.log(isNaN); // => isNaN
-console.log(Array); // => Array
-
-// "Array"という名前の変数を定義
-const Array = 1;
-// 自分で定義した変数がビルトインオブジェクトより優先される
-console.log(Array); // => 1
+console.log(startTime);
