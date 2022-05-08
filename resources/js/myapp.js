a33825f762571de8137578682b9ca21bd333a6e2
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 16:42:14 +0900

    [change] アロー関数のthisはトップレベルに書かれたthisと同じ値になる

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,17 +1,10 @@
 "use strict";
 
-const Prefixer = {
-    prefix: "pre",
-    prefixArray(strings) {
-        return strings.map((str) => {
-            // Arrow Function自体は`this`を持たない
-            // `this`は外側の`prefixArray`関数が持つ`this`を参照する
-            // そのため`this.prefix`は"pre"となる
-            return this.prefix + "-" + str;
-        });
-    }
+// Arrow Functionで定義した関数
+const fn = () => {
+    // この関数の外側には関数は存在しない
+    // トップレベルの`this`と同じ値
+    return this;
 };
-// このとき、`prefixArray`のベースオブジェクトは`Prefixer`となる
-// つまり、`prefixArray`メソッド内の`this`は`Prefixer`を参照する
-const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
-console.log(prefixedStrings); // => ["pre-a", "pre-b", "pre-c"]
+
+console.log(fn() === this); // => true
