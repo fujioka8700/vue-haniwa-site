fc5e3b9d44e49b602e34a03cbeb0af8635be1c5a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 15:39:24 +0900

    [change] メソッド呼び出しにおけるthis

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,13 +1,66 @@
-"use strict"
+"use strict";
 
-function countUp() {
-    // countプロパティを参照して変更する
-    countUp.count = countUp.count + 1;
-    return countUp.count;
+function fn1() {
+    return this;
 }
-countUp.count = 0;
-// 呼び出すごとにcountが更新される
-console.log(countUp()); // => 1
-// 直接値を変更できてしまう
-countUp.count = 10;
-console.log(countUp()); // => 11
+const fn2 = function() {
+    return this;
+};
+// 関数の中の`this`が参照する値は呼び出し方によって決まる
+// `fn1`と`fn2`どちらもただの関数として呼び出している
+// メソッドとして呼び出していないためベースオブジェクトはない
+// ベースオブジェクトがない場合、`this`は`undefined`となる
+console.log(fn1()); // => undefined
+console.log(fn2()); // => undefined
+
+
+function outer() {
+    console.log(this); // => undefined
+    function inner() {
+        console.log(this); // => undefined
+    }
+    // `inner`関数呼び出しのベースオブジェクトはない
+    inner();
+}
+
+// `outer`関数呼び出しのベースオブジェクトはない
+outer();
+
+
+const obj = {
+    // 関数式をプロパティの値にしたメソッド
+    method1: function() {
+        return this;
+    },
+    // 短縮記法で定義したメソッド
+    method2() {
+        return this;
+    }
+};
+// メソッド呼び出しの場合、それぞれの`this`はベースオブジェクト(`obj`)を参照する
+// メソッド呼び出しの`.`の左にあるオブジェクトがベースオブジェクト
+console.log(obj.method1()); // => obj
+console.log(obj.method2()); // => obj
+
+const person = {
+    fullName: "Brendan Eich",
+    sayName: function() {
+        // `person.fullName`と書いているのと同じ
+        return this.fullName;
+    }
+};
+// `person.fullName`を出力する
+console.log(person.sayName()); // => "Brendan Eich"
+
+
+const obj1 = {
+    obj2: {
+        obj3: {
+            method() {
+                return this;
+            }
+        }
+    }
+};
+// `obj1.obj2.obj3.method`メソッドの`this`は`obj3`を参照
+console.log(obj1.obj2.obj3.method() === obj1.obj2.obj3); // => true
