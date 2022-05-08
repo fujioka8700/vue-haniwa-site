e55a03172125219f4eb9e0db205e6a6b87a1f5a9
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 16:02:26 +0900

    [change] callを使ったthisの扱い

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,25 +1,12 @@
 "use strict";
 
+function say(message) {
+    return `${message} ${this.fullName}！`;
+}
 const person = {
-    fullName: "Brendan Eich",
-    sayName: function() {
-        // `this`は呼び出し元によって異なる
-        return this.fullName;
-    }
+    fullName: "Brendan Eich"
 };
-// `sayName`メソッドは`person`オブジェクトに所属する
-// `this`は`person`オブジェクトとなる
-console.log(person.sayName()); // => "Brendan Eich"
-// `person.sayName`を`say`変数に代入する
-const say = person.sayName;
-// 代入したメソッドを関数として呼ぶ
-// この`say`関数はどのオブジェクトにも所属していない
-// `this`はundefinedとなるため例外を投げる
-// say(); // => TypeError: Cannot read property 'fullName' of undefined
-console.log(say);
-// const say = person.sayName; は次のようなイメージ
-const say2 = function() {
-    return this.fullName;
-};
-// `this`は`undefined`となるため例外を投げる
-// say2(); // => TypeError: Cannot read property 'fullName' of undefined
+// `this`を`person`にして`say`関数を呼びだす
+console.log(say.call(person, "こんにちは")); // => "こんにちは Brendan Eich！"
+// `say`関数をそのまま呼び出すと`this`は`undefined`となるため例外が発生
+say("こんにちは"); // => TypeError: Cannot read property 'fullName' of undefined
