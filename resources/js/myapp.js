54ee00544e14450763072ffed3c0a88b1fc2e434
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 12:16:42 +0900

    [change] クロージャーがなぜ動くのか

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,39 +1,40 @@
 "use strict"
 
-// `increment`関数を定義して返す関数
-function createCounter() {
+function printX() {
+    const x = "X";
+    console.log(x); // => "X"
+}
+
+printX();
+// この時点で`"X"`を参照するものはなくなる -> 解放される
+
+function createArray() {
+    const tempArray = [1, 2, 3];
+    return tempArray;
+}
+const array = createArray();
+console.log(array); // => [1, 2, 3]
+
+
+const createCounter = () => {
     let count = 0;
-    // `increment`関数は`count`変数を参照
-    function increment() {
+    return function increment() {
+        // `increment`関数は`createCounter`関数のスコープに定義された`変数`count`を参照している
         count = count + 1;
         return count;
-    }
-    return increment;
-}
-// `myCounter`は`createCounter`が返した関数を参照
+    };
+};
+// createCounter()の実行結果は、内側で定義されていた`increment`関数
 const myCounter = createCounter();
-console.log(myCounter);
+// myCounter関数の実行結果は`count`の評価結果
 console.log(myCounter()); // => 1
 console.log(myCounter()); // => 2
-// 新しく`newCounter`を定義する
-const newCounter = createCounter();
-console.log(newCounter()); // => 1
-console.log(newCounter()); // => 2
-// `myCounter`と`newCounter`は別々の状態持っている
-console.log(myCounter()); // => 3
-console.log(newCounter()); // => 3
 
-
-const x = 10; // ＊1
-
-function printX() {
-    // この識別子`x`は常に ＊1 の変数`x`を参照する
-    console.log(x); // => 10
-}
-
-function run() {
-    const x = 20; // ＊2
-    printX(); // 常に10が出力される
-}
-
-run();
+// countUpとnewCountUpはそれぞれ別のincrement関数(内側にあるのも別のcount変数)
+const countUp = createCounter();
+const newCountUp = createCounter();
+// 参照してる関数(オブジェクト)は別であるため===は一致しない
+console.log(countUp === newCountUp);// false
+// それぞれの状態も別となる
+console.log(countUp()); // => 1
+console.log(newCountUp()); // => 1
