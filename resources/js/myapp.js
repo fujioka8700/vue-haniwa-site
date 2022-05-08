f07dd355c70ab76b89fc1ad52f850ddec491b4fb
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 11:59:02 +0900

    [change] 静的スコープ

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,39 @@
 "use strict"
 
-// 匿名関数を宣言 + 実行を同時に行っている
-window.foo = (function() {
-    // 関数のスコープ内でfoo変数を宣言している
-    var foo = "foo";
-    console.log(foo); // => "foo"
-})();
-// foo変数のスコープ外
-console.log(typeof foo === "undefined"); // => true
+// `increment`関数を定義して返す関数
+function createCounter() {
+    let count = 0;
+    // `increment`関数は`count`変数を参照
+    function increment() {
+        count = count + 1;
+        return count;
+    }
+    return increment;
+}
+// `myCounter`は`createCounter`が返した関数を参照
+const myCounter = createCounter();
+console.log(myCounter);
+console.log(myCounter()); // => 1
+console.log(myCounter()); // => 2
+// 新しく`newCounter`を定義する
+const newCounter = createCounter();
+console.log(newCounter()); // => 1
+console.log(newCounter()); // => 2
+// `myCounter`と`newCounter`は別々の状態持っている
+console.log(myCounter()); // => 3
+console.log(newCounter()); // => 3
+
+
+const x = 10; // ＊1
+
+function printX() {
+    // この識別子`x`は常に ＊1 の変数`x`を参照する
+    console.log(x); // => 10
+}
+
+function run() {
+    const x = 20; // ＊2
+    printX(); // 常に10が出力される
+}
+
+run();
