e1883a40214f77a62a8a97b0cbd647aaa4aaa40e
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 21:27:20 +0900

    [change] class構文と関数でのクラスの違い

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,15 +1,30 @@
 "use strict";
 
-// 非推奨の例: コンストラクタで値を返すべきではない
-class Point {
-    constructor(x, y) {
-        // `this`の代わりにただのオブジェクトを返せる
-        return { x, y };
-    }
-}
+// コンストラクタ関数
+const Point = function PointConstructor(x, y) {
+    // インスタンスの初期化処理
+    this.x = x;
+    this.y = y;
+    // return {
+    //     x : x,
+    //     y : y
+    // }
+};
 
-// `new`演算子の結果はコンストラクタ関数が返したただのオブジェクト
+// `new`演算子でコンストラクタ関数から新しいインスタンスを作成
 const point = new Point(3, 4);
-console.log(point); // => { x: 3, y: 4 }
-// Pointクラスのインスタンスではない
-console.log(point instanceof Point); // => false
+console.log(point);
+console.log(point instanceof Point);
+
+
+// 関数でのクラス表現
+function MyClassLike() {
+}
+// 関数なので関数として呼び出せる
+MyClassLike();
+
+// `class`構文でのクラス
+class MyClass {
+}
+// クラスは関数として呼び出すと例外が発生する
+MyClass(); 
