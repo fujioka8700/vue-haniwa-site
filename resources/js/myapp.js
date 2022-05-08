1ae77e6fa28b4b3635e9b5ed82a40cc3158e5f30
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 11:38:23 +0900

    [change] 即時実行関数

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,40 +1,10 @@
 "use strict"
 
-let let_x;
-var var_x;
-// 宣言後にそれぞれの変数を参照すると`undefined`となる
-console.log(let_x); // => undefined
-console.log(var_x); // => undefined
-// 宣言後に値を代入できる
-let_x = "letのx";
-var_x = "varのx";
-
-console.log(x); // => ReferenceError: can't access lexical declaration `x' before initialization
-// let x = "letのx";
-
-// var宣言より前に参照してもエラーにならない
-console.log(x); // => undefined
-var x = "varのx";
-
-function fn() {
-    // 内側のスコープにあるはずの変数`x`が参照できる
-    console.log(x); // => undefined
-    {
-        var x = "varのx";
-    }
-    console.log(x); // => "varのx"
-}
-fn();
-
-// 解釈されたコード
-function fn2() {
-    // もっとも近い関数スコープの先頭に宣言部分が巻き上げられる
-    var x;
-    console.log(x); // => undefined
-    {
-        // 変数への代入はそのままの位置に残る
-        x = "varのx";
-    }
-    console.log(x); // => "varのx"
-}
-fn2();
+// 匿名関数を宣言 + 実行を同時に行っている
+window.foo = (function() {
+    // 関数のスコープ内でfoo変数を宣言している
+    var foo = "foo";
+    console.log(foo); // => "foo"
+})();
+// foo変数のスコープ外
+console.log(typeof foo === "undefined"); // => true
