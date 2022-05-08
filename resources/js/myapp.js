3193a4b163a721872b38d79cf45f2cdb8010fa53
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 16:58:46 +0900

    [change] Arrow Functionはthisをbindできない

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,15 +1,11 @@
 "use strict";
 
-function outer() {
-    const that = this;
-    // Arrow Functionで定義した関数を返す
-    return () => {
-        // この関数の外側には`outer`関数が存在する
-        // `outer`関数に`this`を書いた場合と同じ
-        return that;
-    };
-}
+const fn = () => {
+    return this;
+};
 
-// `outer`関数の返り値はArrow Functionにて定義された関数
-const innerArrowFunction = outer();
-console.log(innerArrowFunction()); // => undefined
+// Scriptコンテキストの場合、スクリプト直下のArrow Functionの`this`はグローバルオブジェクト
+console.log(fn()); // グローバルオブジェクト
+
+// callで`this`を`{}`にしようとしても、`this`は変わらない
+console.log(fn.call({}) === this); // グローバルオブジェクト
