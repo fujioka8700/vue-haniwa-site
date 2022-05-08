6c99d68e33c8ac0471c59cf140b2131388e85df9
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 16:08:46 +0900

    [change] bindを使ったthisの扱い

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -6,16 +6,6 @@ function say(message) {
 const person = {
     fullName: "Brendan Eich"
 };
-// `this`を`person`にして`say`関数を呼びだす
-// callとは異なり引数を配列として渡す
-console.log(say.apply(person, ["こんにちは"])); // => "こんにちは Brendan Eich！"
-// `say`関数をそのまま呼び出すと`this`は`undefined`となるため例外が発生
-// say("こんにちは"); // => TypeError: Cannot read property 'fullName' of undefined
-
-
-function add(x, y) {
-    return x + y;
-}
-// `this`が不要な場合は、nullを渡す
-console.log(add.call(null, 1, 2)); // => 3
-console.log(add.apply(null, [1, 2])); // => 3
+// `this`を`person`に束縛した`say`関数をラップした関数を作る
+const sayPerson = say.bind(person, "こんにちは");
+console.log(sayPerson()); // => "こんにちは Brendan Eich！"
