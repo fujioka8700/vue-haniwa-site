51380f3ae977c2e019d37e2813a9f84b88b7827c
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 16:31:18 +0900

    [change] mapメソッドへthisを引数として渡す

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -3,14 +3,12 @@
 const Prefixer = {
     prefix: "pre",
     prefixArray(strings) {
-        // `that`は`prefixArray`メソッド呼び出しにおける`this`となる
-        // つまり`that`は`Prefixer`オブジェクトを参照する
-        const that = this;
+        // Arrayの`map`メソッドは第二引数に`this`となる値を渡せる
         return strings.map(function(str) {
-            console.log(that.prefix);
-            // `this`ではなく`that`を参照する
-            return that.prefix + "-" + str;
-        });
+            // `this`が第二引数の値と同じになる
+            // つまり`prefixArray`メソッドと同じ`this`となる
+            return this.prefix + "-" + str;
+        }, this);
     }
 };
 // `prefixArray`メソッドにおける`this`は`Prefixer`
