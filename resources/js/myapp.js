a95a0bc1cdc9965f098226b064460a937d087958
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 11:28:30 +0900

    [change] 関数スコープとvarの巻き上げ

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,27 +1,40 @@
 "use strict"
 
-function doHeavyTask() {
-    for (let i = 0; i < 1000; i++) {
-        console.log("Hello");        
-    }
-}
-const startTime = Date.now();
-doHeavyTask();
-const endTime = Date.now();
-console.log(`実行時間は${endTime - startTime}ミリ秒`);
+let let_x;
+var var_x;
+// 宣言後にそれぞれの変数を参照すると`undefined`となる
+console.log(let_x); // => undefined
+console.log(var_x); // => undefined
+// 宣言後に値を代入できる
+let_x = "letのx";
+var_x = "varのx";
 
-// 実行時間を計測したい関数をコールバック関数として引数に渡す
-const measureTask2 = (taskFn) => {
-    const startTime = Date.now();
-    taskFn();
-    const endTime = Date.now();
-    console.log(`実行時間は${endTime - startTime}ミリ秒`);
-};
-function doHeavyTask2() {
-    for (let i = 0; i < 1000; i++) {
-        console.log("Hello");        
-    }
-}
-measureTask2(doHeavyTask2);
+console.log(x); // => ReferenceError: can't access lexical declaration `x' before initialization
+// let x = "letのx";
 
-console.log(startTime);
+// var宣言より前に参照してもエラーにならない
+console.log(x); // => undefined
+var x = "varのx";
+
+function fn() {
+    // 内側のスコープにあるはずの変数`x`が参照できる
+    console.log(x); // => undefined
+    {
+        var x = "varのx";
+    }
+    console.log(x); // => "varのx"
+}
+fn();
+
+// 解釈されたコード
+function fn2() {
+    // もっとも近い関数スコープの先頭に宣言部分が巻き上げられる
+    var x;
+    console.log(x); // => undefined
+    {
+        // 変数への代入はそのままの位置に残る
+        x = "varのx";
+    }
+    console.log(x); // => "varのx"
+}
+fn2();
