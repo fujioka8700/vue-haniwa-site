7d0b3fead6c141c30a0e33ef9d1fad7f0fb48165
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-08 16:07:21 +0900

    [change] applyを使ったthisの扱い

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -7,6 +7,15 @@ const person = {
     fullName: "Brendan Eich"
 };
 // `this`を`person`にして`say`関数を呼びだす
-console.log(say.call(person, "こんにちは")); // => "こんにちは Brendan Eich！"
+// callとは異なり引数を配列として渡す
+console.log(say.apply(person, ["こんにちは"])); // => "こんにちは Brendan Eich！"
 // `say`関数をそのまま呼び出すと`this`は`undefined`となるため例外が発生
-say("こんにちは"); // => TypeError: Cannot read property 'fullName' of undefined
+// say("こんにちは"); // => TypeError: Cannot read property 'fullName' of undefined
+
+
+function add(x, y) {
+    return x + y;
+}
+// `this`が不要な場合は、nullを渡す
+console.log(add.call(null, 1, 2)); // => 3
+console.log(add.apply(null, [1, 2])); // => 3
