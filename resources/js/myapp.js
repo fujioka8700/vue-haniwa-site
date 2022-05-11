52c32c1cdb1f250cb809937bdf8afc7bd710d1db
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 15:52:46 +0900

    [change] プロトタイプオブジェクト

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -2,14 +2,16 @@
 
 class MyClass {
     constructor() {
-        console.log(this);
+        this.method=()=>{
+
+        }
     }
     method() { }
 }
 
 console.log(MyClass.prototype.method);
 console.log(typeof MyClass.prototype.method === "function"); // => true
-new MyClass();
-console.log(MyClass.prototype.constructor);
+const hoge = new MyClass();
+console.log(hoge.method);
 // クラスのconstructorはクラス自身を参照する
 console.log(MyClass.prototype.constructor === MyClass); // => true
