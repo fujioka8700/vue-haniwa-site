30558664f879350cd71c6e9c6c6d931d4c87617a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 18:46:00 +0900

    [change] SyntaxError

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,11 +1,11 @@
 'use strict';
 
 try {
-    // 存在しない変数を参照する
-    console.log(x);
-    
+    // eval関数は渡した文字列をJavaScriptとして実行する関数
+    // 正しくない構文をパースさせ、SyntaxErrorを実行時に発生させる
+    eval("foo! bar!");
 } catch (error) {
-    console.log(error instanceof ReferenceError); // => true
-    console.log(error.name); // => "ReferenceError"
+    console.log(error instanceof SyntaxError); // => true
+    console.log(error.name); // => "SyntaxError"
     console.log(error.message); // エラーメッセージが表示される
 }
