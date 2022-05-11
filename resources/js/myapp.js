ed88205c7d8cb6324436fe40c2eeec1c3072cbc4
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 14:15:19 +0900

    [change] クラスのアクセッサプロパティの定義

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,25 @@
 "use strict";
 
-class PrototypeClass {
-    method() {
-        // `this`はベースオブジェクトを参照する
-        return this;
-    };
+class NumberWrapper {
+    constructor(value) {
+        this._value = value;
+    }
+    // `_value`プロパティの値を返すgetter
+    get value() {
+        console.log("getter");
+        return this._value;
+    }
+    // `_value`プロパティに値を代入するsetter
+    set value(newValue) {
+        console.log("setter");
+        this._value = newValue;
+    }
 }
 
-const instance = new PrototypeClass();
-const method = instance.method;
-
-// ベースオブジェクトはundefined
-console.log(method()); // => undefined
+const numberWrapper = new NumberWrapper(1);
+// "getter"とコンソールに表示される
+console.log(numberWrapper.value); // => 1
+// "setter"とコンソールに表示される
+numberWrapper.value = 42;
+// "getter"とコンソールに表示される
+console.log(numberWrapper.value); // => 42
