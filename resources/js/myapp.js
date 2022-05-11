440fbb1e4fc2d7cd2f7350d203eeb58319ed9ec6
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 10:58:30 +0900

    [change] プロトタイプメソッドは呼び出し方によってthisの参照先が異なる

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,23 +1,14 @@
 "use strict";
 
-class ArrowClass {
-    constructor() {
-        
-        // コンストラクタでの`this`は常にインスタンス
-        this.method = () => {
-
-            // Arrow Functionにおける`this`は静的に決まる
-            // そのため`this`は常にインスタンスを参照する
-            return this;
-        };
-    }
+class PrototypeClass {
+    method() {
+        // `this`はベースオブジェクトを参照する
+        return this;
+    };
 }
 
-const instanceA = new ArrowClass();
-const methodA = instanceA.method;
+const instance = new PrototypeClass();
+const method = instance.method;
 
-// 呼び出し方法（ベースオブジェクト）に依存しないため、`this`がインスタンスを参照する
-console.log(methodA()); // => instance
-
-const instanceB = new ArrowClass();
-console.log(methodA() === instanceB.method());
+// ベースオブジェクトはundefined
+console.log(method()); // => undefined
