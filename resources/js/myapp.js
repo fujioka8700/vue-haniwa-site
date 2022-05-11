b0535634fc4ee3ece1e5f25d655edda2a01ce504
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-12 06:29:16 +0900

    [change] 同期処理ではフリーズしてしまう

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,8 +1,16 @@
 'use strict';
 
-function fn() {
-    console.log("メッセージ");
-    console.error("エラーメッセージ");
+// 指定した`timeout`ミリ秒経過するまで同期的にブロックする関数
+function blockTime(timeout) {
+    const startTime = Date.now();
+    // `timeout`ミリ秒経過するまで無限ループをする
+    while (true) {
+        const diffTime = Date.now() - startTime;
+        if (diffTime >= timeout) {
+            return; // 指定時間経過したら関数の実行を終了
+        }
+    }
 }
-
-fn();
+console.log("処理を開始");
+blockTime(1000); // 他の処理を1000ミリ秒（1秒間）ブロックする
+console.log("この行が呼ばれるまで処理が1秒間ブロックされる");
