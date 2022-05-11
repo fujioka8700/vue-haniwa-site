bef9df2f995b413a352dc061c58a3b85e4cd1a25
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 19:03:26 +0900

    [change] ビルトインエラーを投げる

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,11 +1,19 @@
 'use strict';
 
+// 文字列を反転する関数
+function reverseString(str) {
+    if (typeof str !== "string") {
+        throw new TypeError(`${str} is not a string`);
+    }
+    return Array.from(str).reverse().join("");
+}
+
 try {
-    // 関数ではないオブジェクトを関数として呼び出す
-    const fn = {};
-    fn();
+    // 数値を渡す
+    const str = reverseString(100);
+    console.log(str);
 } catch (error) {
     console.log(error instanceof TypeError); // => true
     console.log(error.name); // => "TypeError"
-    console.log(error.message); // エラーメッセージが表示される
+    console.log(error.message); // "100 is not a string"
 }
