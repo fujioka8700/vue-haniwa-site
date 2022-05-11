f1d6319b7547ea997e9df0d418007a74f4a86c47
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 18:29:56 +0900

    [change] catchとfainallyは省略できる

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,21 +1,18 @@
 'use strict';
 
+// catch節のみ
 try {
-    console.log("try節:この行は実行されます");
-
-    // 未定義の関数を呼び出してReferenceError例外が発生する
     undefinedFunction();
-
-    // 例外が発生したため、この行は実行されません
-    console.log("Hello World!");
 } catch (error) {
-
-    // 例外が発生したあとはこのブロックが実行される
-    console.log("catch節:この行は実行されます");
-    console.log(error instanceof ReferenceError); // => true
-    console.log(error.message); // => "undefinedFunction is not defined"
-} finally {
-
-    // このブロックは例外の発生に関係なく必ず実行される
-    console.log("finally節:この行は実行されます");
+    console.error(error);
 }
+
+// finally節のみ
+try {
+    undefinedFunction();
+} finally {
+    console.log("この行は実行されます");
+}
+
+// finally節のみでは例外がキャッチされないため、この行は実行されません
+console.log("Hello World");
