2a853e02b2b03233ec986ca59dcfd93d09da3160
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 17:33:14 +0900

    [change] superプロパティの扱い

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,9 +1,22 @@
 'use strict';
 
 class Parent {
-    static hello() {
-        return "Hello";
+    method() {
+        console.log("Parent.prototype.method");
     }
 }
-class Child extends Parent {}
-console.log(Child.hello()); // => "Hello"
+
+class Child extends Parent {
+    method() {
+        console.log("Child.prototype.method");
+        // `this.method()`だと自分(`this`)のmethodを呼び出して無限ループする
+        // そのため明示的に`super.method()`を呼ぶことで、Parent.prototype.methodを呼び出す
+        super.method();
+    }
+}
+
+const child = new Child();
+child.method();
+// コンソールには次のように出力される
+// "Child.prototype.method"
+// "Parent.prototype.method"
