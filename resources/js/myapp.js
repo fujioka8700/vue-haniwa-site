9a4a74b6c4195afb396c7e8d7ac6e40536dddadf
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 17:37:02 +0900

    [change] Parentを継承したChildから親クラスの静的メソッドを呼び出す

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,22 +1,18 @@
 'use strict';
 
 class Parent {
-    method() {
-        console.log("Parent.prototype.method");
+    static method() {
+        console.log("Parent.method");
     }
 }
-
 class Child extends Parent {
-    method() {
-        console.log("Child.prototype.method");
-        // `this.method()`だと自分(`this`)のmethodを呼び出して無限ループする
-        // そのため明示的に`super.method()`を呼ぶことで、Parent.prototype.methodを呼び出す
+    static method() {
+        console.log("Child.method");
+        // `super.method()`で`Parent.method`を呼びだす
         super.method();
     }
 }
-
-const child = new Child();
-child.method();
+Child.method();
 // コンソールには次のように出力される
-// "Child.prototype.method"
-// "Parent.prototype.method"
+// "Child.method"
+// "Parent.method"
