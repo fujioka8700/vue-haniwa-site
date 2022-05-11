b251f0fd727b1eb5e9f8ccf92bda57c973b1083a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 17:39:37 +0900

    [change] 継承の判定 instanceof

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,18 +1,15 @@
 'use strict';
 
-class Parent {
-    static method() {
-        console.log("Parent.method");
-    }
-}
-class Child extends Parent {
-    static method() {
-        console.log("Child.method");
-        // `super.method()`で`Parent.method`を呼びだす
-        super.method();
-    }
-}
-Child.method();
-// コンソールには次のように出力される
-// "Child.method"
-// "Parent.method"
+class Parent {}
+class Child extends Parent {}
+
+const parent = new Parent();
+const child = new Child();
+
+// `Parent`のインスタンスは`Parent`のみを継承したインスタンス
+console.log(parent instanceof Parent); // => true
+console.log(parent instanceof Child); // => false
+
+// `Child`のインスタンスは`Child`と`Parent`を継承したインスタンス
+console.log(child instanceof Parent); // => true
+console.log(child instanceof Child); // => true
