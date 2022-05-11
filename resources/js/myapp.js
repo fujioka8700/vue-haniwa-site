6bb06189220569acbb43eaf4b075f43570791319
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 18:37:15 +0900

    [change] 文字列を例外として投げるアンチパターンの例

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,16 +1,9 @@
 'use strict';
 
-// 渡された数値が0以上ではない場合に例外を投げる関数
-function assertPositiveNumber(num) {
-    if (num < 0) {
-        throw new Error(`${num} is not positive.`);
-    }
-}
-
+// 文字列を例外として投げるアンチパターンの例
 try {
-    // 0未満の値を渡しているので、関数が例外を投げる
-    assertPositiveNumber(-1);
+    throw "例外が投げられました";
 } catch (error) {
-    console.log(error instanceof Error); // => true
-    console.log(error.message); // => "-1 is not positive."
+    // catch節の例外識別子は、投げられた値を参照する
+    console.log(error); // => "例外が投げられました"
 }
