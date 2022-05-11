0a2a543f42fd9265d4d61fd2ed623c479903c3cc
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 16:48:46 +0900

    [change] プロトタイプチェーンの仕組みを疑似的なコードとして表現する

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,6 +1,12 @@
 'use strict';
 
+// プロトタイプチェーンの動作の疑似的なコード
 class MyClass {
+    constructor() {
+        this.method =()=> {
+            console.log("インスタンスのメソッド"); 
+        };
+    }
     method() {
         console.log("プロトタイプのメソッド");
     }
@@ -8,11 +14,19 @@ class MyClass {
 
 const instance = new MyClass();
 
-// インスタンスには`method`プロパティがないため、プロトタイプオブジェクトの`method`が参照される
-instance.method(); // "プロトタイプのメソッド"
+// `instance.method()`を実行する場合
+// 次のような呼び出し処理が行われている
+// インスタンスが`method`プロパティを持っている場合
+if (Object.hasOwn(instance, "method")) {
+    instance.method();
+} else {
+    // インスタンスの`[[Prototype]]`の参照先（`MyClass`のプロトタイプオブジェクト）を取り出す
+    const prototypeObject = Object.getPrototypeOf(instance);
 
-// `instance.method`の参照はプロトタイプオブジェクトの`method`と一致する
-const Prototype = Object.getPrototypeOf(instance);
-console.log(Prototype.method);
-console.log(instance.method);
-console.log(instance.method === Prototype.method); // => true
+    // プロトタイプオブジェクトが`method`プロパティを持っている場合
+    if (Object.hasOwn(prototypeObject, "method")) {
+        
+        // `this`はインスタンス自身を指定して呼び出す
+        prototypeObject.method.call(instance);
+    }
+}
