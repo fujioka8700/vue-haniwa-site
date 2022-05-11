c5e9c9a14fafb82c36c33cdd6db07987ab706b0a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 18:24:19 +0900

    [change] 基本的な例外処理

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,20 +1,21 @@
 'use strict';
 
-class MyArray extends Array {
-    get first() {
-        console.log(this);
-        return this.at(0);
-    }
+try {
+    console.log("try節:この行は実行されます");
 
-    get last() {
-        return this.at(-1);
-    }
+    // 未定義の関数を呼び出してReferenceError例外が発生する
+    undefinedFunction();
+
+    // 例外が発生したため、この行は実行されません
+    console.log("Hello World!");
+} catch (error) {
+
+    // 例外が発生したあとはこのブロックが実行される
+    console.log("catch節:この行は実行されます");
+    console.log(error instanceof ReferenceError); // => true
+    console.log(error.message); // => "undefinedFunction is not defined"
+} finally {
+
+    // このブロックは例外の発生に関係なく必ず実行される
+    console.log("finally節:この行は実行されます");
 }
-
-// Arrayを継承しているのでArray.fromも継承している
-// Array.fromはIterableなオブジェクトから配列インスタンスを作成する
-const array = MyArray.from([1, 2, 3, 4, 5]);
-console.log(array instanceof Array); // => true
-console.log(array.length); // => 5
-console.log(array.first); // => 1
-console.log(array.last); // => 5
