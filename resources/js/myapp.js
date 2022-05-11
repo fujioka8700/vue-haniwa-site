08d26f5d4dfada6e395481a3c3b6487e47733251
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 15:26:17 +0900

    [change] クラスの静的メソッドにおけるthisは、そのクラス自身を参照する。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,12 +1,15 @@
+'use strict';
+
 class ArrayWrapper {
     constructor(array = []) {
         this.array = array;
     }
 
-	// 静的メソッド
-    // rest parametersとして要素を受けつける
     static of(...items) {
-        return new ArrayWrapper(items);
+        // `this`は`ArrayWrapper`を参照する
+        const hoge = new this(items);
+        console.log(hoge);
+        return hoge;
     }
 
     get length() {
@@ -14,12 +17,5 @@ class ArrayWrapper {
     }
 }
 
-// 配列を引数として渡している
-const arrayWrapperA = new ArrayWrapper([1, 2, 3]);
-
-// 静的メソッドの呼び出し
-// 要素を引数として渡している
-const arrayWrapperB = ArrayWrapper.of(1, 2, 3);
-
-console.log(arrayWrapperA.length); // => 3
-console.log(arrayWrapperB.length); // => 3
+const arrayWrapper = ArrayWrapper.of(1, 2, 3);
+console.log(arrayWrapper.length); // => 3
