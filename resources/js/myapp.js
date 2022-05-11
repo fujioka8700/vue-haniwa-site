baffa91c9488814b90b12dfd53620593e57a5b25
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 17:23:22 +0900

    [change] プロトタイプ継承

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,22 +1,16 @@
 'use strict';
 
 class Parent {
-    constructor() {
-        this.name = "Parent";
+    method() {
+        console.log("Parent.prototype.method");
     }
 }
 
+// `Parent`を継承した`Child`を定義
 class Child extends Parent {
-    constructor() {
-        // 子クラスでは`super()`を`this`に触る前に呼び出さなければならない
-        super();
-        // 子クラスのコンストラクタ処理
-        // 親クラスで書き込まれた`name`は上書きされる
-        this.name = "Child";
-    }
+    // methodの定義はない
 }
 
-const parent = new Parent();
-console.log(parent.name); // => "Parent"
-const child = new Child();
-console.log(child.name); // => "Child"
+// `Child`のインスタンスは`Parent`のプロトタイプメソッドを継承している
+const instance = new Child();
+instance.method(); // "Parent.prototype.method"
