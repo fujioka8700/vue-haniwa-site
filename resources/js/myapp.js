5a03a2235c54fbec276efce404ed208d84dbc897
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-11 17:25:39 +0900

    [change] 静的メソッドの継承

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,16 +1,9 @@
 'use strict';
 
 class Parent {
-    method() {
-        console.log("Parent.prototype.method");
+    static hello() {
+        return "Hello";
     }
 }
-
-// `Parent`を継承した`Child`を定義
-class Child extends Parent {
-    // methodの定義はない
-}
-
-// `Child`のインスタンスは`Parent`のプロトタイプメソッドを継承している
-const instance = new Child();
-instance.method(); // "Parent.prototype.method"
+class Child extends Parent {}
+console.log(Child.hello()); // => "Hello"
