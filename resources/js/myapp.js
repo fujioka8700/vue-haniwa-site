1239907e2c6607593a32138634568213b8051a37
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 08:48:56 +0900

    [change] 非同期処理はメインスレッドで実行される

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -11,12 +11,13 @@ function blockTime(timeout) {
     }
 }
 
-console.log("1. setTimeoutのコールバック関数を10ミリ秒後に実行します");
+const startTime = Date.now();
+// 10ミリ秒後にコールバック関数を呼び出すようにタイマーに登録する
 setTimeout(() => {
-    console.log("3. ブロックする処理を開始します");
-    blockTime(1000); // 他の処理を1秒間ブロックする
-    console.log("4. ブロックする処理が完了しました");
+    const endTime = Date.now();
+    console.log(`非同期処理のコールバックが呼ばれるまで${endTime - startTime}ミリ秒かかりました`);
 }, 10);
 
-// ブロックする処理は非同期なタイミングで呼び出されるので、次の行が先に実行される
-console.log("2. 同期的な処理を実行します");
+console.log("ブロックする処理を開始します");
+blockTime(1000); // 1秒間処理をブロックする
+console.log("ブロックする処理が完了しました");
