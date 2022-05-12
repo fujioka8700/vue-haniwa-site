f7e579d6e33ec503d3e14e46b112894458a4aa4a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 08:54:15 +0900

    [change] 非同期処理では例外をキャッチできない

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,23 +1,11 @@
 'use strict';
 
-// 指定した`timeout`ミリ秒経過するまで同期的にブロックする関数
-function blockTime(timeout) {
-    const startTime = Date.now();
-    while (true) {
-        const diffTime = Date.now() - startTime;
-        if (diffTime >= timeout) {
-            return; // 指定時間経過したら関数の実行を終了
-        }
-    }
+try {
+    setTimeout(() => {
+        throw new Error("非同期的なエラー");
+    }, 10);
+} catch (error) {
+    // 非同期エラーはキャッチできないため、この行は実行されません
+    console.log(error.message);
 }
-
-const startTime = Date.now();
-// 10ミリ秒後にコールバック関数を呼び出すようにタイマーに登録する
-setTimeout(() => {
-    const endTime = Date.now();
-    console.log(`非同期処理のコールバックが呼ばれるまで${endTime - startTime}ミリ秒かかりました`);
-}, 10);
-
-console.log("ブロックする処理を開始します");
-blockTime(1000); // 1秒間処理をブロックする
-console.log("ブロックする処理が完了しました");
+console.log("この行は実行されます");
