6fd51b03f5b6a40b89714177a2372c353ba79fae
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 08:57:43 +0900

    [change] コールバック関数内で同期的なエラーとしてキャッチする必要がある

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,11 +1,13 @@
 'use strict';
 
-try {
-    setTimeout(() => {
-        throw new Error("非同期的なエラー");
-    }, 10);
-} catch (error) {
-    // 非同期エラーはキャッチできないため、この行は実行されません
-    console.log(error.message);
-}
+// 非同期処理の外
+setTimeout(() => {
+    // 非同期処理の中
+    try {
+        throw new Error("エラー");
+    } catch (error) {
+        console.log("エラーをキャッチできる");
+    }
+}, 10);
+
 console.log("この行は実行されます");
