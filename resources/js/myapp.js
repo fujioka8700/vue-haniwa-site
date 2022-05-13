c465a1b622783e9cfa1a84e6e07ce587922fbcc8
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 09:49:02 +0900

    [change] コールバック関数は、関数によって処理を変えたい時に使う

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,30 +1,30 @@
 'use strict';
 
 /**
- * リソースの取得に成功した場合は`successCallback(レスポンス)`を呼び出す
- * リソースの取得に失敗した場合は`failureCallback(エラー)`を呼び出す
+ * コールバック関数は、関数によって処理を変えたい時に使う
  */
- function dummyFetch(path, successCallback, failureCallback) {
+ function dummyFetch(path, callback) {
     setTimeout(() => {
         if (path.startsWith("/success")) {
-            successCallback({ body: `Response body of ${path}` });
+            callback(null, { body: `Response body of ${path}` });
         } else {
-            failureCallback(new Error("NOT FOUND"));
+            callback(new Error("NOT FOUND"));
         }
     }, 1000 * Math.random());
 }
 
-function successCallback(response) {
-    console.log(response); // => { body: "Response body of /success/data" }
-};
+dummyFetch("/success/data", (error, response) => {
+    if (error) {
+        // この行は実行されません
+    } else {
+        console.log(response); // => { body: "Response body of /success/data" }
+    }
+});
 
-function failureCallback(error) {
-    console.log(error.message); // => "NOT FOUND"
-}
-
-// /success/data にリソースが存在するので、`response`にはデータが入る
-dummyFetch("/success/data", successCallback, failureCallback);
-
-// /failure/data にリソースは存在しないので、`error`にはエラーオブジェクトが入る
-dummyFetch("/failure/data", successCallback, failureCallback);
-
+dummyFetch("/failure/data", (error, response )=>{
+    if (error) {
+        console.log(error.message); // => NOT FOUND
+    } else {
+        // この行は実行されません
+    }
+});
