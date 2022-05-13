0ab8f404048ac4aa24ec85c2c4e9d3832b93d023
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 18:07:28 +0900

    [change] mainで発生したエラーのログを取りながら、Promiseチェーンはエラーのまま処理を継続できる。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,11 +1,17 @@
 'use strict';
 
-Promise.resolve().then(function onFulfilledA() {
-    return Promise.reject(new Error("失敗"));
-}).then(function onFulfilledB() {
-    console.log("onFulfilledBは呼び出されません");
-}).catch(function onRejected(error) {
-    console.log(error.message); // => "失敗"
-}).then(function onFulfilledC() {
-    console.log("onFulfilledCは呼び出されます");
+function main() {
+    return Promise.reject(new Error("エラー"));
+}
+
+// mainはRejectedなPromiseを返す
+main().catch(error => {
+    // mainで発生したエラーのログを出力する
+    console.log(error);
+    // Promiseチェーンはそのままエラーを継続させる
+    return Promise.reject(error);
+}).then(() => {
+    // 前のcatchでRejectedなPromiseが返されたため、この行は実行されません
+}).catch(error => {
+    console.log("メインの処理が失敗した");
 });
