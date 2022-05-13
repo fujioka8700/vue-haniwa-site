8bf4227c7e277d6c3adeb83eb958a1dbea844343
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 15:51:40 +0900

    [change] Promise内で例外が発生するとエラー時のコールバック関数が呼び出される

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,17 +1,13 @@
 'use strict';
 
-function errorPromise(message) {
+function throwPromise() {
     return new Promise((resolve, reject) => {
-        reject(new Error(message));
+        // Promiseコンストラクタの中で例外は自動的にキャッチされrejectを呼ぶ
+        throw new Error("例外が発生");
+        // 例外が発生すると、これ以降のコンストラクタの処理は実行されません
     });
 }
 
-// 非推奨: `then`メソッドで失敗時のコールバック関数だけを登録
-errorPromise("thenでエラーハンドリング").then(undefined, (error) => {
-    console.log(error.message); // => "thenでエラーハンドリング"
-});
-
-// 推奨: `catch`メソッドで失敗時のコールバック関数を登録
-errorPromise("catchでエラーハンドリング").catch(error => {
-    console.log(error.message); // => "catchでエラーハンドリング"
+throwPromise().catch(error => {
+    console.log(error.message); // => "例外が発生"
 });
