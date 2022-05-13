b434a382e04b266ae2bd40da0e39295283f55395
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 09:34:51 +0900

    [change] 2種類のコールバック関数を受け取る形に変更

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,36 +1,30 @@
 'use strict';
 
 /**
- * 1000ミリ秒未満のランダムなタイミングでレスポンスを疑似的にデータ取得する関数
- * 指定した`path`にデータがある場合は`callback(null, レスポンス)`を呼ぶ
- * 指定した`path`にデータがない場合は`callback(エラー)`を呼ぶ
+ * リソースの取得に成功した場合は`successCallback(レスポンス)`を呼び出す
+ * リソースの取得に失敗した場合は`failureCallback(エラー)`を呼び出す
  */
- function dummyFetch(path, callback) {
+ function dummyFetch(path, successCallback, failureCallback) {
     setTimeout(() => {
-        // /success からはじまるパスにはリソースがあるという設定
         if (path.startsWith("/success")) {
-            callback(null, { body: `Response body of ${path}` });
+            successCallback({ body: `Response body of ${path}` });
         } else {
-            callback(new Error("NOT FOUND"));
+            failureCallback(new Error("NOT FOUND"));
         }
     }, 1000 * Math.random());
 }
 
+function successCallback(response) {
+    console.log(response); // => { body: "Response body of /success/data" }
+};
+
+function failureCallback(error) {
+    console.log(error.message); // => "NOT FOUND"
+}
+
 // /success/data にリソースが存在するので、`response`にはデータが入る
-dummyFetch("/success/data", (error, response) => {
-    if (error) {
-        // この行は実行されません
-    } else {
-        console.log(response); // => { body: "Response body of /success/data" }
-    }
-});
+dummyFetch("/success/data", successCallback, failureCallback);
 
 // /failure/data にリソースは存在しないので、`error`にはエラーオブジェクトが入る
-dummyFetch("/failure/data", (error, response) => {
-    if (error) {
-        console.log(error.message); // => "NOT FOUND"
-    } else {
-        // この行は実行されません
-    }
-});
+dummyFetch("/failure/data", successCallback, failureCallback);
 
