da9fb0dd23b4a44cc85f9fae842a08e272afaf19
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 17:29:34 +0900

    [change] RejectedなPromiseは次の失敗時の処理までスキップする

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,21 +1,12 @@
 'use strict';
 
-// ランダムでFulfilledまたはRejectedの`Promise`インスタンスを返す関数
-function asyncTask() {
-    return Math.random() > 0.5
-        ? Promise.resolve("成功")
-        : Promise.reject(new Error("失敗"));
-}
+// RejectedなPromiseは次の失敗時の処理までスキップする
+const rejectedPromise = Promise.reject(new Error("失敗"));
 
-// asyncTask関数は新しい`Promise`インスタンスを返す
-asyncTask()
-    // thenメソッドは新しい`Promise`インスタンスを返す
-    .then(function onFulfilled(value) {
-        console.log(value); // => "成功"
-    })
-    // catchメソッドは新しい`Promise`インスタンスを返す
-    .catch(function onRejected(error) {
-        console.log(error.message); // => "失敗"
-    });
-
-console.log("同期的な処理");
+rejectedPromise.then(() => {
+    // このthenのコールバック関数は呼び出されません
+}).then(() => {
+    // このthenのコールバック関数は呼び出されません
+}).catch(error => {
+    console.log(error.message); // => "失敗"
+});
