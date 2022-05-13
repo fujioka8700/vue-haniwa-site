aaf1c80e192be5ec52dae89bad53dfeff3ae292f
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 17:50:11 +0900

    [change] Promiseチェーンで一度キャッチすると、次に呼ばれるのは成功時の処理となる。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,11 @@
 'use strict';
 
-Promise.resolve(1).then((value) => {
+Promise.reject(new Error("失敗")).catch(error => {
+    // 一度catchすれば、次に呼ばれるのは成功時のコールバック
+    return 1;
+}).then(value => {
     console.log(value); // => 1
     return value * 2;
 }).then(value => {
     console.log(value); // => 2
-    return value * 2;
-}).then(value => {
-    console.log(value); // => 4
-    // 値を返さない場合は undefined を返すのと同じ
-}).then(value => {
-    console.log(value); // => undefined
 });
