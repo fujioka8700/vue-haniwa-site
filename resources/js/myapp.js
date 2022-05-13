9200791a368a53b2a5c69e0cecadaa729d6fdb3b
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 16:34:34 +0900

    [change] コンストラクタ関数、同期的な処理、非同期的な処理の順で呼ばれる

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const promise = Promise.resolve();
-
-// Fulfilledの状態となったPromiseインスタンスに対しても
-// thenメソッドでコールバック関数を登録できます。
-promise.then(() => {
-    console.log("2. コールバック関数が実行されました");
+const promise = new Promise((resolve) => {
+    console.log("1. resolveします");
+    resolve();
 });
-console.log("1. 同期的な処理が実行されました");
+
+promise.then(() => {
+    console.log("3. コールバック関数が実行されました");
+});
+
+console.log("2. 同期的な処理が実行されました");
