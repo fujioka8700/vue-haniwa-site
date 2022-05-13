2d756f28a51c6115b235662b28b5124158ff2de5
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-13 09:16:54 +0900

    [change] エラーファーストコールバック

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,13 +1,36 @@
 'use strict';
 
-// 非同期処理の外
-setTimeout(() => {
-    // 非同期処理の中
-    try {
-        throw new Error("エラー");
-    } catch (error) {
-        console.log("エラーをキャッチできる");
-    }
-}, 10);
+/**
+ * 1000ミリ秒未満のランダムなタイミングでレスポンスを疑似的にデータ取得する関数
+ * 指定した`path`にデータがある場合は`callback(null, レスポンス)`を呼ぶ
+ * 指定した`path`にデータがない場合は`callback(エラー)`を呼ぶ
+ */
+ function dummyFetch(path, callback) {
+    setTimeout(() => {
+        // /success からはじまるパスにはリソースがあるという設定
+        if (path.startsWith("/success")) {
+            callback(null, { body: `Response body of ${path}` });
+        } else {
+            callback(new Error("NOT FOUND"));
+        }
+    }, 1000 * Math.random());
+}
 
-console.log("この行は実行されます");
+// /success/data にリソースが存在するので、`response`にはデータが入る
+dummyFetch("/success/data", (error, response) => {
+    if (error) {
+        // この行は実行されません
+    } else {
+        console.log(response); // => { body: "Response body of /success/data" }
+    }
+});
+
+// /failure/data にリソースは存在しないので、`error`にはエラーオブジェクトが入る
+dummyFetch("/failure/data", (error, response) => {
+    if (error) {
+        console.log(error.message); // => "NOT FOUND"
+    } else {
+        // この行は実行されません
+    }
+});
+
