8f26636bca865e267497070029b319de025d5908
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 05:24:04 +0900

    [change] マップのdeleteとclear

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -2,15 +2,12 @@
 
 const map = new Map();
 
-// 新しい要素の追加
-map.set("key", "value1");
+map.set("key1", "value1");
+map.set("key2", "value2");
+console.log(map.size); // => 2
+
+map.delete("key1");
 console.log(map.size); // => 1
-console.log(map.get("key")); // => "value1"
 
-// 要素の上書き
-map.set("key", "value2");
-console.log(map.get("key")); // => "value2"
-
-// キーの存在確認
-console.log(map.has("key")); // => true
-console.log(map.has("foo")); // => false
+map.clear();
+console.log(map.size); // => 0
