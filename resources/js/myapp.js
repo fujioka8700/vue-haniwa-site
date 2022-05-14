7b5276729a4ff7260abe2a2d253495ad1b01fd73
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 05:17:48 +0900

    [change] マップの初期値を設定

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,4 +1,9 @@
 'use strict';
 
-const map = new Map();
-console.log(map.size); // => 0
+const map = new Map([
+    ["key1", "value1"],
+    ["key2", "value2"]
+]);
+
+// 2つのエントリーで初期化されている
+console.log(map.size); // => 2
