a899621a7e7e70238f6fad2c91c0b3d15cb242ec
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 05:28:00 +0900

    [change] マップの反復処理

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const map = new Map();
+const map = new Map([
+    ["key1", "value1"],
+    ["key2", "value2"]
+]);
+const results = [];
 
-map.set("key1", "value1");
-map.set("key2", "value2");
-console.log(map.size); // => 2
-
-map.delete("key1");
-console.log(map.size); // => 1
-
-map.clear();
-console.log(map.size); // => 0
+map.forEach((value, key) => {
+    results.push(`${key}:${value}`);
+});
+console.log(results); // => ["key1:value1","key2:value2"]
