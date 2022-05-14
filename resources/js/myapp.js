c1c92840108606349c387c78bc71ba6bbe7c49d1
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-14 12:25:29 +0900

    [chage] コンソールログを入れてみると動作がわかりやすい。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -15,18 +15,21 @@ function dummyFetch(path) {
 // リソースを順番に取得する
 async function fetchResources(resources) {
     const results = [];
-    // コールバック関数をAsync Functionに変更
+    console.log("1. fetchResourcesを開始");
     resources.forEach(async function(resource) {
-        // await式を利用できるようになった
+        console.log(`2. ${resource}の取得開始`);
         const response = await dummyFetch(resource);
+        // `dummyFetch`が完了するのは、`fetchResources`関数が返したPromiseが解決された後
+        console.log(`5. ${resource}の取得完了`);
         results.push(response.body);
     });
+    console.log("3. fetchResourcesを終了");
     return results;
 }
 const resources = ["/resource/A", "/resource/B"];
 
 // リソースを取得して出力する
 fetchResources(resources).then((results) => {
-    // しかし、resultsは空になってしまう
+    console.log("4. fetchResourcesの結果を取得");
     console.log(results); // => []
 });
