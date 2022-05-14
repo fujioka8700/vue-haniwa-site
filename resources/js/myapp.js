d83938ed96e4959eeffa7e196f361ff2eb276e6b
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-14 11:55:01 +0900

    [chage] Async Function外の処理は停止しない。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,7 +1,17 @@
 'use strict';
 
-// asyncではない関数では`await`式は利用できない
-function main(){
-    // SyntaxError: await is only valid in async functions
-    await Promise.resolve();
+async function asyncMain() {
+    // 中でawaitしても、Async Functionの外側の処理まで止まるわけではない
+    await new Promise((resolve) => {
+        setTimeout(resolve, 3000);
+    });
 }
+console.log("1. asyncMain関数を呼び出します");
+
+// Async Functionは外から見れば単なるPromiseを返す関数
+asyncMain().then(() => {
+    console.log("3. asyncMain関数が完了しました");
+});
+
+// Async Functionの外側の処理はそのまま進む
+console.log("2. asyncMain関数外では、次の行が同期的に呼び出される");
