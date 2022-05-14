daee42df2aa31b3798a1f53279d3441579ca66eb
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-14 10:51:56 +0900

    [chage] await式で発生した例外をtry...catch構文でキャッチすると、Promise.catchメソッドは実行されない。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,12 +1,17 @@
 'use strict';
 
 async function asyncMain() {
-    // `await`式で評価した右辺のPromiseがRejectedとなったため、例外がthrowされる
-    const value = await Promise.reject(new Error("エラーメッセージ"));
-    // await式で例外が発生したため、この行は実行されません
+    // await式のエラーはtry...catchできる
+    try {
+        // `await`式で評価した右辺のPromiseがRejectedとなったため、例外がthrowされる
+        const value = await Promise.reject(new Error("エラーメッセージ"));
+        // await式で例外が発生したため、この行は実行されません
+    } catch (error) {
+        console.log(error.message); // => "エラーメッセージ"
+    }
 }
 
-// Async Functionは自動的に例外をキャッチできる
+// asyncMainはResolvedなPromiseを返す
 asyncMain().catch(error => {
-    console.log(error.message); // => "エラーメッセージ"
+    // すでにtry...catchされているため、この行は実行されません
 });
