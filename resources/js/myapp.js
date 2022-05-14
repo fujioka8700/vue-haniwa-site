062fb2f1313ea1025a03ed4cbd13132d7c6a8a90
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 08:57:08 +0900

    [change] Setでは重複する同じ値を持たないことを保証するため、同じ値は1つのみ格納される。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,4 +1,7 @@
 'use strict';
 
-const set = new Set();
-console.log(set.size); // => 0
+// "value2"が重複するため、片方は無視される
+const set = new Set(["value1", "value2", "value2"]);
+
+// セットのサイズは2になる
+console.log(set.size); // => 2
