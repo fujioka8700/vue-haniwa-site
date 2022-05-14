efb5cfed2a155960641c35a4c2737bde4c1331cd
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 05:14:28 +0900

    [change] マップの作成

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,4 @@
 'use strict';
 
-function doAsyncTask() {
-    return new Promise(resolve => setTimeout(resolve, 1000));
-}
-
-(async function() {
-    console.log("実行開始");
-    const startTime = Date.now();
-
-    // awaitを使う処理
-    const result = await doAsyncTask();
-    console.log(`実行終了: ${Date.now() - startTime}ms 経過しました`);
-})();
+const map = new Map();
+console.log(map.size); // => 0
