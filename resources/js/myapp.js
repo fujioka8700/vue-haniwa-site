7c8ab66c67b9a08e61b331282706a8395e538a28
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 05:20:38 +0900

    [change] マップの要素の追加と確認

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,9 +1,16 @@
 'use strict';
 
-const map = new Map([
-    ["key1", "value1"],
-    ["key2", "value2"]
-]);
+const map = new Map();
 
-// 2つのエントリーで初期化されている
-console.log(map.size); // => 2
+// 新しい要素の追加
+map.set("key", "value1");
+console.log(map.size); // => 1
+console.log(map.get("key")); // => "value1"
+
+// 要素の上書き
+map.set("key", "value2");
+console.log(map.get("key")); // => "value2"
+
+// キーの存在確認
+console.log(map.has("key")); // => true
+console.log(map.has("foo")); // => false
