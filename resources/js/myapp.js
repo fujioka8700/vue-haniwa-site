a02ffb8ea849120fdf083894b98f03c0520fa84e
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-14 11:08:39 +0900

    [chage] await式を使って逐次処理を行う

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -13,18 +13,18 @@ function dummyFetch(path) {
 }
 
 // リソースAとリソースBを順番に取得する
-function fetchAB() {
+async function fetchAB() {
     const results = [];
-    return dummyFetch("/resource/A").then(response => {
-        results.push(response.body);
-        return dummyFetch("/resource/B");
-    }).then(response => {
-        results.push(response.body);
-        return results;
-    });
+    const responseA = await dummyFetch("/resource/A");
+    results.push(responseA.body);
+    const responseB = await dummyFetch("/resource/B");
+    results.push(responseB.body);
+    return results;
 }
 
 // リソースを取得して出力する
 fetchAB().then((results) => {
     console.log(results); // => ["Response body of /resource/A", "Response body of /resource/B"]
+}).catch(error => {
+    console.error(error.message);
 });
