c7a9f17271c99d3a874ee0aede2b840f6be85f54
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 08:48:02 +0900

    [change] あるオブジェクトから計算した結果を一時的に保存する用途でもよく使われる。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,32 +1,13 @@
 'use strict';
 
-// イベントリスナーを管理するマップ
-const listenersMap = new WeakMap();
+const cache = new WeakMap();
 
-class EventEmitter {
-    addListener(listener) {
-        // this にひもづいたリスナーの配列を取得する
-        const listeners = listenersMap.get(this) ?? [];
-        const newListeners = listeners.concat(listener);
-        // this をキーに新しい配列をセットする
-        listenersMap.set(this, newListeners);
+function getHeight(element) {
+    if (cache.has(element)) {
+        return cache.get(element);
     }
+    const height = element.getBoundingClientRect().height;
+    // elementオブジェクトに対して高さをひもづけて保存している
+    cache.set(element, height);
+    return height;
 }
-
-// 上記クラスの実行例
-
-let eventEmitter = new EventEmitter();
-
-// イベントリスナーを追加する
-eventEmitter.addListener(() => {
-    console.log("イベントが発火しました");
-});
-
-eventEmitter.addListener(() => {
-    console.log("イベントが発火しました");
-});
-
-// eventEmitterへの参照がなくなったことで自動的にイベントリスナーが解放される
-eventEmitter = null;
-
-console.log(listenersMap);
