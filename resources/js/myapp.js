f2fbfb0a6680750aad14f840d76fdec2294a369a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 11:50:26 +0900

    [change] 小数点以下を単純に切り捨てる

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// 底関数
-console.log(Math.floor(1.3)); // => 1
-console.log(Math.floor(-1.3)); // => -2
-
-// 天井関数
-console.log(Math.ceil(1.3)); // => 2
-console.log(Math.ceil(-1.3)); // => -1
-
-// 四捨五入
-console.log(Math.round(1.3)); // => 1
-console.log(Math.round(1.6)); // => 2
-console.log(Math.round(-1.3)); // => -1
+// 単純に小数部分を切り落とす
+console.log(Math.trunc(1.3)); // => 1
+console.log(Math.trunc(-1.3)); // => -1
