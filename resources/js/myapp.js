0c64971f5723353b6282c681a63b4985d0fd3ad2
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 11:19:01 +0900

    [change] Dateインスタンスが持つ時刻が不正な場合。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,12 +1,6 @@
 'use strict';
 
-// 実行環境における"2006年1月2日15時04分05秒999"を表す
-// タイムゾーンを指定することはできない
-const date1 = new Date(2006, 0, 2, 15, 4, 5, 999);
-console.log(date1.toISOString()); // "2006-01-02T06:04:05.999Z" (Asia/Tokyoの場合)
-
-// Date.UTCメソッドを使うとUTCに固定できる
-const ms = Date.UTC(2006, 0, 2, 15, 4, 5, 999);
-// 時刻値を渡すコンストラクタと併用する
-const date2 = new Date(ms);
-console.log(date2.toISOString()); // => "2006-01-02T15:04:05.999Z"
+// 不正なDateインスタンスを作成する
+const invalid = new Date("");
+console.log(invalid.getTime()); // => NaN
+console.log(invalid.toString()); // => "Invalid Date"
