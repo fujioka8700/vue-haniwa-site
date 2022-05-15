5ca5b83ac7bd4c6385a45146619869578e662cae
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 19:55:31 +0900

    [change] 分割代入

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,22 +1,54 @@
 'use strict';
 
-function sum(...values) {
-    return values.reduce((total, value)=>{
-        return total + Number(value);
+function echo(x, y) {
+  return [x, y];
+}
+
+console.log(echo(1));
+
+function addPrefix(text, prefix) {
+    const pre = prefix ?? "デフォルト値:";
+    return pre + text;
+}
+
+console.log(addPrefix("文字列", "カスタム値："));
+console.log(addPrefix("文字列"));
+
+function add(...values) {
+    return values.reduce((total, value) => {
+        return total + value;
     }, 0);
 }
 
-const x = 1, z = 10;
-let y;
-console.log(sum(x, y, z));
+console.log(add(1, 2));
+console.log(add(1, 2, 3));
 
-console.log(10 + NaN);
+console.log(Math.max(10, 20, 30));
 
-function isEmptyString(str) {
-    // return !Boolean(str); // falsyな値が判別できない
-    return typeof str === "string" && str.length === 0;
+function fu() {
+  for (const value of arguments) {
+      console.log(value);
+  }
 }
 
-console.log(isEmptyString(""));
-console.log(isEmptyString(0));
-console.log(isEmptyString());
+fu("a", "b", "c");
+
+function printUserId({ name, id }) {
+    console.log(`名前：${name}`);
+    console.log(`ID:${id}`);
+}
+
+const user = {
+    name : "nanao",
+    id : 42
+}
+
+printUserId(user);
+
+function print([first, second]) {
+  console.log(first);
+  console.log(second)
+}
+
+const array = [10, 20];
+print(array);
