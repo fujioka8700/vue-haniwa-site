ace1808d5b9dd961f72ee210eadf8f80f7ad2c04
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 19:09:03 +0900

    [change] NaNについて

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,27 +1,14 @@
 'use strict';
 
-// const input = window.prompt("数字を入力してください");
-const input = 100;
-console.log(input);
+const x = 10;
+const y = x + NaN;
+console.log(y);
+console.log(typeof y);
 
-const num = Number(input);
-console.log(typeof num);
-
-const n2 = Number.parseInt("42px", 10);
-console.log(n2);
-console.log(typeof n2);
-
-const n3 = Number.parseFloat("1.5");
-console.log(n3);
-console.log(typeof n3);
-
-const n4 = Number("文字列");
-console.log(n4);
-
-console.log(Number(undefined));
-
-const userInput = "aaa";
-const num5 = Number.parseInt(userInput, 10);
-if (Number.isNaN(num5)) {
-    console.log("パースした結果NaNになった", num5);
+function isNaN(x) {
+  return x === x;
 }
+
+console.log(isNaN(NaN));
+
+console.log(Number.isNaN(NaN));
