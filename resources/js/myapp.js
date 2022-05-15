321506934f8889ef72c27e2fa9e47d08b2ff56c2
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 11:15:13 +0900

    [change] コンストラクタ関数に2つ以上の引数を渡すと、このオーバーロードが適用される。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,12 @@
 'use strict';
 
-// UTCにおける"2006年1月2日15時04分05秒999"を表すISO 8601形式の文字列
-const inUTC = new Date("2006-01-02T15:04:05.999Z");
-console.log(inUTC.toISOString()); // => "2006-01-02T15:04:05.999Z"
+// 実行環境における"2006年1月2日15時04分05秒999"を表す
+// タイムゾーンを指定することはできない
+const date1 = new Date(2006, 0, 2, 15, 4, 5, 999);
+console.log(date1.toISOString()); // "2006-01-02T06:04:05.999Z" (Asia/Tokyoの場合)
 
-// 上記の例とは異なり、UTCであることを表す'Z'がついていないことに注意
-// Asia/Tokyo(+09:00)で実行すると、UTCにおける表記は9時間前の06時04分05秒になる
-const inLocal = new Date("2006-01-02T15:04:05.999");
-console.log(inLocal.toISOString()); // "2006-01-02T06:04:05.999Z" (Asia/Tokyoの場合)
+// Date.UTCメソッドを使うとUTCに固定できる
+const ms = Date.UTC(2006, 0, 2, 15, 4, 5, 999);
+// 時刻値を渡すコンストラクタと併用する
+const date2 = new Date(ms);
+console.log(date2.toISOString()); // => "2006-01-02T15:04:05.999Z"
