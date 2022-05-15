bd4f52bda215609d126f59bcc5696435f208cb11
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 09:57:06 +0900

    [change] JSONにシリアライズできないオブジェクト

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,25 @@
 'use strict';
 
-const obj = { id: 1, name: "js-primer" };
-console.log(JSON.stringify(obj, null, "\t"));
-/*
-{
-   "id": 1,
-   "name": "js-primer"
-}
-*/
+// 値が関数のプロパティ
+console.log(JSON.stringify({ x: function() {} })); // => '{}'
+
+// 値がSymbolのプロパティ
+console.log(JSON.stringify({ x: Symbol("") })); // => '{}'
+
+// 値がundefinedのプロパティ
+console.log(JSON.stringify({ x: undefined })); // => '{}'
+
+// 配列の場合
+console.log(JSON.stringify({ x: [10, function() {}] })); // => '{"x":[10,null]}'
+
+// キーがSymbolのプロパティ
+JSON.stringify({ [Symbol("foo")]: "foo" }); // => '{}'
+
+// 値がRegExpのプロパティ
+console.log(JSON.stringify({ x: /foo/ })); // => '{"x":{}}'
+
+// 値がMapのプロパティ
+const map = new Map();
+
+map.set("foo", "foo");
+console.log(JSON.stringify({ x: map })); // => '{"x":{}}'
