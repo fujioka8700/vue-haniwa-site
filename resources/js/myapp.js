d00a706a9f071a200b6d4619195f582ebe734be5
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 09:04:40 +0900

    [change] SetのforEachメソッド。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const set = new Set();
-set.add("a");
-set.add("b");
-console.log(set.size); // => 2
-set.delete("a");
-console.log(set.size); // => 1
-set.clear();
-console.log(set.size); // => 0
+const set = new Set(["a", "b"]);
+const results = [];
+
+set.forEach((value) => {
+    results.push(value);
+});
+console.log(results); // => ["a","b"]
