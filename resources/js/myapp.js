035aadcff3b4589b1722eca32a4ed2d0cbad255a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 09:44:44 +0900

    [change] 配列に含まれる名前のプロパティだけが変換される。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,10 +1,5 @@
 'use strict';
 
 const obj = { id: 1, name: "js-primer", bio: null };
-const replacer = (key, value) => {
-    if (value === null) {
-        return undefined;
-    }
-    return value;
-};
+const replacer = ["id", "name"];
 console.log(JSON.stringify(obj, replacer)); // => '{"id":1,"name":"js-primer"}'
