7a18d94df50d69de561850da4863e4ee23674646
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 11:02:49 +0900

    [change] Dateのコンストラクタに、文字列型の引数を渡す。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,8 +1,10 @@
 'use strict';
 
-// 時刻のミリ秒値を直接指定する形式
-// 1136214245999はUTCにおける"2006年1月2日15時04分05秒999"を表す
-const date = new Date(1136214245999);
+// UTCにおける"2006年1月2日15時04分05秒999"を表すISO 8601形式の文字列
+const inUTC = new Date("2006-01-02T15:04:05.999Z");
+console.log(inUTC.toISOString()); // => "2006-01-02T15:04:05.999Z"
 
-// 末尾の'Z'はUTCであることを表す
-console.log(date.toISOString()); // => "2006-01-02T15:04:05.999Z"
+// 上記の例とは異なり、UTCであることを表す'Z'がついていないことに注意
+// Asia/Tokyo(+09:00)で実行すると、UTCにおける表記は9時間前の06時04分05秒になる
+const inLocal = new Date("2006-01-02T15:04:05.999");
+console.log(inLocal.toISOString()); // "2006-01-02T06:04:05.999Z" (Asia/Tokyoの場合)
