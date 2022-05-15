7f14067947ac24cee4a7be027b53b71bd85e8fcb
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 11:29:59 +0900

    [change] DateインスタンスのgetTimezoneOffsetメソッド。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,13 +1,10 @@
 'use strict';
 
-// YYYY/MM/DD形式の文字列に変換する関数
-function formatDate(date) {
-    const yyyy = String(date.getFullYear());
-    // Stringの`padStart`メソッド（ES2017）で2桁になるように0埋めする
-    const mm = String(date.getMonth() + 1).padStart(2, "0");
-    const dd = String(date.getDate()).padStart(2, "0");
-    return `${yyyy}/${mm}/${dd}`;
-}
+// getTimezoneOffsetはインスタンスメソッドなので、インスタンスが必要
+const now = new Date();
 
-const date = new Date("2006-01-02T15:04:05.999");
-console.log(formatDate(date)); // => "2006/01/02"
+// 時間単位にしたタイムゾーンオフセット
+const timezoneOffsetInHours = now.getTimezoneOffset() / 60;
+
+// UTCの現在の時間を計算できる
+console.log(`Hours in UTC: ${now.getHours() + timezoneOffsetInHours}`);
