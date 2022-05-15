089cfb07f17e3afb07bc67386df978834f24f739
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 09:11:13 +0900

    [change] Setのkeysメソッドとentriesメソッド。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const set = new Set(["a", "b"]);
-const results = [];
 
-set.forEach((value) => {
-    results.push(value);
-});
-console.log(results); // => ["a","b"]
+// keysで列挙
+const keysResults = [];
+for (const value of set.keys()) {
+    keysResults.push(value);
+}
+console.log(keysResults); // => ["a","b"]
+
+// entriesで列挙
+const entryResults = [];
+for (const entry of set.entries()) {
+    // entryは[値, 値]という配列
+    entryResults.push(entry);
+}
+console.log(entryResults); // => [["a","a"], ["b", "b"]]
