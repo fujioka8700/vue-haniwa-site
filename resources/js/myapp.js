d800e9b2e65efaa6e668e45a7364faedd5e29247
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 11:22:14 +0900

    [change] Dateインスタンスの時刻の各部分を取得するメソッド

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,6 +1,13 @@
 'use strict';
 
-// 不正なDateインスタンスを作成する
-const invalid = new Date("");
-console.log(invalid.getTime()); // => NaN
-console.log(invalid.toString()); // => "Invalid Date"
+// YYYY/MM/DD形式の文字列に変換する関数
+function formatDate(date) {
+    const yyyy = String(date.getFullYear());
+    // Stringの`padStart`メソッド（ES2017）で2桁になるように0埋めする
+    const mm = String(date.getMonth() + 1).padStart(2, "0");
+    const dd = String(date.getDate()).padStart(2, "0");
+    return `${yyyy}/${mm}/${dd}`;
+}
+
+const date = new Date("2006-01-02T15:04:05.999");
+console.log(formatDate(date)); // => "2006/01/02"
