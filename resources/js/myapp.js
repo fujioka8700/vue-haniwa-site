fd3e7c097f905521d28abfece664f4d5f19f69ee
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 09:02:11 +0900

    [change] Setのdeleteメソッドとclearメソッド。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,15 +1,10 @@
 'use strict';
 
 const set = new Set();
-
-// 値の追加
 set.add("a");
+set.add("b");
+console.log(set.size); // => 2
+set.delete("a");
 console.log(set.size); // => 1
-
-// 重複する値は追加されない
-set.add("a");
-console.log(set.size); // => 1
-
-// 値の存在確認
-console.log(set.has("a")); // => true
-console.log(set.has("b")); // => false
+set.clear();
+console.log(set.size); // => 0
