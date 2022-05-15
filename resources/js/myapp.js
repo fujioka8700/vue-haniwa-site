a5eef2d1b9097699b2e0f114ac92db40e7666aaa
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 16:31:13 +0900

    [add] グローバル変数を操作するためのモジュール

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -3,8 +3,13 @@
 // すべての名前つきエクスポートをmyModuleオブジェクトとしてまとめてインポートする
 import * as myModule from "./my-module.js";
 
+// ./side-effects.jsのグローバルコードが実行される
+import "./side-effects.js";
+
 // fooとして名前つきエクスポートされた値にアクセスする
 console.log(myModule.foo); // => "foo"
 
 // defaultとしてデフォルトエクスポートされた値にアクセスする
 console.log(myModule.default); // => { baz: "baz" }
+
+console.log(window.hoge);
