86c15bbbb6de19830968717802c95089180585c2
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 09:15:24 +0900

    [change] Setオブジェクト自身もiterableなオブジェクト。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,18 +1,8 @@
 'use strict';
 
 const set = new Set(["a", "b"]);
-
-// keysで列挙
-const keysResults = [];
-for (const value of set.keys()) {
-    keysResults.push(value);
+const results = [];
+for (const value of set) {
+    results.push(value);
 }
-console.log(keysResults); // => ["a","b"]
-
-// entriesで列挙
-const entryResults = [];
-for (const entry of set.entries()) {
-    // entryは[値, 値]という配列
-    entryResults.push(entry);
-}
-console.log(entryResults); // => [["a","a"], ["b", "b"]]
+console.log(results); // => ["a","b"]
