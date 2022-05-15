2b89fd82fcf2342fb5644f7fa2a13f90624ef84f
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-15 19:29:11 +0900

    [change] 空文字列の判別

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,14 +1,22 @@
 'use strict';
 
-const x = 10;
-const y = x + NaN;
-console.log(y);
-console.log(typeof y);
-
-function isNaN(x) {
-  return x === x;
+function sum(...values) {
+    return values.reduce((total, value)=>{
+        return total + Number(value);
+    }, 0);
 }
 
-console.log(isNaN(NaN));
+const x = 1, z = 10;
+let y;
+console.log(sum(x, y, z));
 
-console.log(Number.isNaN(NaN));
+console.log(10 + NaN);
+
+function isEmptyString(str) {
+    // return !Boolean(str); // falsyな値が判別できない
+    return typeof str === "string" && str.length === 0;
+}
+
+console.log(isEmptyString(""));
+console.log(isEmptyString(0));
+console.log(isEmptyString());
