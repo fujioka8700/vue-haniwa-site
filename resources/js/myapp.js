c9197f73f492a91620715587402334095944a867
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-18 11:36:48 +0900

    [change] オブジェクトのマージ

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,100 +1,20 @@
 'use strict';
 
-const name = "名前";
-const key = "key-string";
+const objectA = { a: "a"};
+const objectB = { b: "b"};
 
-const obj = {
-  name,
-  [key] : 23
-};
+const merged = Object.assign({}, objectA, objectB);
 
-console.log(obj);
+console.log(merged);
 
-let obj2 = new Object();
-obj2 = function () {
-  
-}
+const mergedAB = Object.assign(objectA, objectB);
+console.log(mergedAB);
+console.log(objectA);
+console.log(objectB);
+console.log(mergedAB === objectA);
 
-const obj3 = () => {
-  return true;
-}
-console.log(obj3);
+const objectVA = { version: "a" };
+const objectVB = { version: "b" };
 
-obj.key = "value";
-console.log(obj.key);
-console.log(obj[key]);
-
-// 悪い例
-function changeProperty(obj) {
-  obj.key = "値です";
-  delete obj.key;
-};
-
-changeProperty(obj);
-console.log(obj.key);
-
-const object = Object.freeze({key: "value"});
-// object.key = "value2";
-
-console.log(object.key);
-
-// obj = obj2;
-console.log(obj);
-
-const widget = {
-  window: {
-    title: "ウィジェットのタイトル"
-  },
-  size: 1200
-}
-
-if (widget.window !== undefined) {
-  console.log("widget.windowはある");
-}
-
-// console.log(widget.windw);
-// console.log(widget.windw.title);
-
-// inを使ってプロパティの存在を確認
-if ("window" in widget) {
-  console.log("`window`プロパティは存在する");
-}
-
-// Object.hasOwnを使ってプロパティの存在を確認
-if (Object.hasOwn(widget, "window")) {
-  console.log("`window`プロパティは存在する");
-}
-
-// Object.prototype.hasOwnPropertyを使ってプロパティの存在を確認
-if (widget.hasOwnProperty("window")) {
-  console.log("`window`プロパティは存在する");
-}
-
-const titleKey = "title";
-
-console.log(widget?.window?.[titleKey]);
-
-console.log(widget.toString());
-console.log(String(widget));
-
-const CustomObject = {
-  toString() {
-    return "カスタムオブジェト";
-  }
-}
-
-console.log(String(CustomObject));
-const obj4 = {};
-const keyObject = Symbol("シンボル1");
-obj4[keyObject] = "100";
-console.log(obj4[keyObject]);
-console.log(Object.hasOwn(obj4, "a"));
-
-console.log(Object.keys(widget));
-console.log(Object.values(widget));
-console.log(Object.entries(widget));
-
-const keys = Object.keys(widget)
-keys.forEach((key)=>{
-  console.log(key);
-});
+const mergedB = Object.assign({}, objectVA, objectVB);
+console.log(mergedB);
