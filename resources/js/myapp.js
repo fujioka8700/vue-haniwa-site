4f757722a8bb52e5fed7a8acabf6f309069f54f4
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-18 10:09:14 +0900

    [change] ループと反復処理まで。

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,15 +1,84 @@
 'use strict';
 
-const func2 =()=> {
-  console.log("funcです。");
+const fun = function () {
+  console.log("funです");
 }
 
-function dummyFunc(func, number, func2) {
-  func();
-  console.log(number);
-  func2();
+// console.log(fun);
+fun();
+
+const array = [1, 2, 3];
+
+const doubleArray = array.map(value=>{
+  return value * 2;
+});
+
+console.log(doubleArray);
+
+console.log(array.map(()=>{}));
+
+const output = value => console.log(value);
+
+array.forEach(output);
+
+const obj = {
+  name: "太郎",
+  getName:()=> {
+    console.log(this.toString());
+    return this.name;
+  }
 }
 
-dummyFunc(()=>{
-  console.log("dummyFunc実行");
-}, 100, func2);
+function dummyFunc(obj) {
+  console.log(obj.getName());
+}
+
+dummyFunc(obj);
+
+if (undefined === window.undefined) {
+  console.log("undefinedです");
+}
+
+if (false) {
+  console.log("実行されません");
+}
+
+if (null) {
+  console.log("実行されません");
+}
+
+if (NaN) {
+  console.log("実行されません");
+}
+
+const array2 = [10, 20, 30, 40, 50];
+
+// 偶数が含まれているか
+const isPassed = array2.some((value)=>{
+  return value % 2 === 0;
+});
+console.log(isPassed);
+
+let count = 0
+while (count < 100) {
+  count++;
+  if (count === 95) {
+    continue;
+  }
+  console.log(count);
+}
+
+const filteredArray = array2.filter((value)=>{
+  return value % 2 === 0;
+});
+
+console.log(filteredArray);
+
+for (const key of filteredArray) {
+  console.log(key);
+}
+
+const result = array2.reduce((total, num)=>{
+  return total + num;
+}, 0);
+console.log(result);
