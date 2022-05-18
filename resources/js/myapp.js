1aae6c65c7d05c32491adc61f955673fa9762e26
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-18 11:26:36 +0900

    [change] オブジェクトの列挙

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,84 +1,100 @@
 'use strict';
 
-const fun = function () {
-  console.log("funです");
-}
-
-// console.log(fun);
-fun();
-
-const array = [1, 2, 3];
-
-const doubleArray = array.map(value=>{
-  return value * 2;
-});
-
-console.log(doubleArray);
-
-console.log(array.map(()=>{}));
-
-const output = value => console.log(value);
-
-array.forEach(output);
+const name = "名前";
+const key = "key-string";
 
 const obj = {
-  name: "太郎",
-  getName:()=> {
-    console.log(this.toString());
-    return this.name;
+  name,
+  [key] : 23
+};
+
+console.log(obj);
+
+let obj2 = new Object();
+obj2 = function () {
+  
+}
+
+const obj3 = () => {
+  return true;
+}
+console.log(obj3);
+
+obj.key = "value";
+console.log(obj.key);
+console.log(obj[key]);
+
+// 悪い例
+function changeProperty(obj) {
+  obj.key = "値です";
+  delete obj.key;
+};
+
+changeProperty(obj);
+console.log(obj.key);
+
+const object = Object.freeze({key: "value"});
+// object.key = "value2";
+
+console.log(object.key);
+
+// obj = obj2;
+console.log(obj);
+
+const widget = {
+  window: {
+    title: "ウィジェットのタイトル"
+  },
+  size: 1200
+}
+
+if (widget.window !== undefined) {
+  console.log("widget.windowはある");
+}
+
+// console.log(widget.windw);
+// console.log(widget.windw.title);
+
+// inを使ってプロパティの存在を確認
+if ("window" in widget) {
+  console.log("`window`プロパティは存在する");
+}
+
+// Object.hasOwnを使ってプロパティの存在を確認
+if (Object.hasOwn(widget, "window")) {
+  console.log("`window`プロパティは存在する");
+}
+
+// Object.prototype.hasOwnPropertyを使ってプロパティの存在を確認
+if (widget.hasOwnProperty("window")) {
+  console.log("`window`プロパティは存在する");
+}
+
+const titleKey = "title";
+
+console.log(widget?.window?.[titleKey]);
+
+console.log(widget.toString());
+console.log(String(widget));
+
+const CustomObject = {
+  toString() {
+    return "カスタムオブジェト";
   }
 }
 
-function dummyFunc(obj) {
-  console.log(obj.getName());
-}
+console.log(String(CustomObject));
+const obj4 = {};
+const keyObject = Symbol("シンボル1");
+obj4[keyObject] = "100";
+console.log(obj4[keyObject]);
+console.log(Object.hasOwn(obj4, "a"));
 
-dummyFunc(obj);
+console.log(Object.keys(widget));
+console.log(Object.values(widget));
+console.log(Object.entries(widget));
 
-if (undefined === window.undefined) {
-  console.log("undefinedです");
-}
-
-if (false) {
-  console.log("実行されません");
-}
-
-if (null) {
-  console.log("実行されません");
-}
-
-if (NaN) {
-  console.log("実行されません");
-}
-
-const array2 = [10, 20, 30, 40, 50];
-
-// 偶数が含まれているか
-const isPassed = array2.some((value)=>{
-  return value % 2 === 0;
-});
-console.log(isPassed);
-
-let count = 0
-while (count < 100) {
-  count++;
-  if (count === 95) {
-    continue;
-  }
-  console.log(count);
-}
-
-const filteredArray = array2.filter((value)=>{
-  return value % 2 === 0;
-});
-
-console.log(filteredArray);
-
-for (const key of filteredArray) {
+const keys = Object.keys(widget)
+keys.forEach((key)=>{
   console.log(key);
-}
-
-const result = array2.reduce((total, num)=>{
-  return total + num;
-}, 0);
-console.log(result);
+});
