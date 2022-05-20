ccfe10aa5b3e2fc4866b5f740b3eb6419346364a
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-20 09:20:18 +0900

    [change] クラスの練習1

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,29 +1,43 @@
 'use strict';
 
-// `callback`関数を受け取り呼び出す関数
-const callCallback = (callback) => {
-  // `callback`を呼び出す実装
-  callback();
-};
+// const Point = function(x, y) {
+//   this.x = x;
+//   this.y = y;
+// }
 
-const obj = {
-  num: 10,
-  method() {
-      callCallback(()=> {
-          // ここでの `this` は`callCallback`の実装に依存する
-          // `callback()`のように単純に呼び出されるなら`this`は`undefined`になる
-          // Functionの`call`メソッドなどを使って特定のオブジェクトを指定するかもしれない
-          // この問題を回避するために`const that = this`のような一時変数を使う
-          console.log("call");
-          console.log(this);
-      });
-  },
-  method2: function(array) {
-    return array.map((value)=>{
-      return value * this.num;
-    });
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-};
+}
 
-const array = [1, 2, 3];
-console.log(obj.method2(array));
+const point = new Point(3, 4);
+console.log(point);
+
+class MyClass {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    return {x, y};
+  }
+}
+
+// const myclass = class {
+//   constructor() {
+
+//   }
+// }
+
+// class Myclass2 {
+
+// }
+
+const myClass = new MyClass(1,2);
+console.log(myClass);
+console.log(myClass instanceof MyClass);
+
+class Thing {}
+const thing = new Thing();
+
+const thing2 = class {};
