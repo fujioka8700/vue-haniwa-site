eeefaf37fd30f2a515948afbc63d58aefe9d30ab
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-20 09:51:38 +0900

    [change] 関数宣言と関数式の違いはFunctionオブジェクトが作成されるタイミング

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,43 +1,62 @@
 'use strict';
 
-// const Point = function(x, y) {
+class Counter {
+  constructor () {
+    this.count = 0;
+    this.increment =()=> {
+      this.count++;
+    }
+  }
+  // increment () {
+  //   this.count++;
+  // }
+}
+
+// const MyClass = function (x) {
 //   this.x = x;
-//   this.y = y;
-// }
 
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-const point = new Point(3, 4);
-console.log(point);
-
-class MyClass {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    return {x, y};
-  }
-}
-
-// const myclass = class {
-//   constructor() {
-
+//   increment () {
+//     console.log("val")
 //   }
 // }
 
-// class Myclass2 {
+const counterA = new Counter();
+const counterB = new Counter();
+counterA.increment();
+counterA.increment();
+console.log(counterA.count);
+console.log(counterB.count);
 
-// }
+console.log(counterA.increment === counterB.increment);
 
-const myClass = new MyClass(1,2);
-console.log(myClass);
-console.log(myClass instanceof MyClass);
+function func() {
+  return "funcです。";
+}
 
-class Thing {}
-const thing = new Thing();
+const func2 =()=> {
+  return "funcです。";
+}
 
-const thing2 = class {};
+console.log(typeof func);
+console.log(typeof func2);
+
+// 関数宣言は、その手前でも呼び出せる
+{
+  var result = func1(1);
+  var name = func1.name;
+  alert(name + ": " + result);
+  // 出力⇒func1: これはfunc1です。値は1
+
+  function func1(n) {
+    return "これはfunc1です。値は" + n;
+  }
+}
+
+// 関数式は、その手前では呼び出せない
+{
+  var result_NG = func2(2); // × 文法エラー
+
+  // var func2 = function (n) {
+  //   return "これはfunc2です。値は" + n;
+  // }
+}
