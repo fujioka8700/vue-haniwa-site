78ddeac8c6dbfe466030f2f936904189297b40d5
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-20 12:46:04 +0900

    [change] 例外処理

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,43 +1,13 @@
 'use strict';
 
-class Parent {
-  constructor (...args) {
-    console.log("Parentコンストラクタの処理:", ...args)
-  }
-  method() {
-    console.log("Parent.prototype.method");
-  }
-  static method() {
-    return "methodです";
-  }
+try {
+  undefinedFunction();
+  // console.log(undefinedFunction());
+  // throw new Error("カスタムエラー");
+} catch (error) {
+  console.log(error.message);
+} finally {
+  console.log("実行される");
 }
 
-class Child extends Parent {
-  constructor (...args) {
-    super(...args);
-    console.log("Childコンストラクタの処理:", ...args);
-    console.log(super.constructor);
-  }
-}
-
-const child = new Child("引数1", "引数2");
-const parent = new Parent("引数1", "引数2");
-child.method();
-
-console.log(Child.method());
-
-console.log(child instanceof Child);
-console.log(parent instanceof Child);
-
-class MyArray extends Array {
-  get first() {
-    return this.at(0);
-  }
-  get last() {
-    return this.at(-1);
-  }
-}
-
-const array = MyArray.from([1,2,3,4,5]);
-console.log(array.first);
-console.log(array.last);
+console.log("例外処理");
