10b56486cc78afb9087da36cafc23e9aafbe01c7
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-20 10:08:24 +0900

    [change] クラスの練習2

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,62 +1,42 @@
 'use strict';
 
 class Counter {
-  constructor () {
+  constructor (value) {
     this.count = 0;
+    this._value = value;
     this.increment =()=> {
       this.count++;
+    };
+    this.method =()=> {
+      return this;
     }
   }
   // increment () {
-  //   this.count++;
+  //     this.count++;
   // }
-}
-
-// const MyClass = function (x) {
-//   this.x = x;
-
-//   increment () {
-//     console.log("val")
-//   }
-// }
-
-const counterA = new Counter();
-const counterB = new Counter();
-counterA.increment();
-counterA.increment();
-console.log(counterA.count);
-console.log(counterB.count);
-
-console.log(counterA.increment === counterB.increment);
-
-function func() {
-  return "funcです。";
-}
-
-const func2 =()=> {
-  return "funcです。";
-}
-
-console.log(typeof func);
-console.log(typeof func2);
-
-// 関数宣言は、その手前でも呼び出せる
-{
-  var result = func1(1);
-  var name = func1.name;
-  alert(name + ": " + result);
-  // 出力⇒func1: これはfunc1です。値は1
-
-  function func1(n) {
-    return "これはfunc1です。値は" + n;
+  method2() {
+    return this;
+  }
+  get value() {
+    console.log("getter");
+    return this._value;
+  }
+  set value(newValue) {
+    this._value = newValue;
   }
 }
 
-// 関数式は、その手前では呼び出せない
-{
-  var result_NG = func2(2); // × 文法エラー
+const counter = new Counter(5);
+const counterB = new Counter(10);
+counter.increment();
+console.log(counter.count); // => 1
+console.log(counter.increment === counterB.increment); // => true
+console.log(counter.method === counterB.method);
+console.log(counter.method2 === counterB.method2);
+console.log(counter.value);
+counter.value = 20;
+console.log(counter.value);
 
-  // var func2 = function (n) {
-  //   return "これはfunc2です。値は" + n;
-  // }
-}
+const array = [1, 2, 3, 4, 5];
+array.length = 2;
+console.log(array.join(", "));
