d8f6368aa698d33f783b4bef7b543e1ade6462fa
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-20 11:09:36 +0900

    [change] クラスの継承

--- resources/js/myapp.js
+++ resources/js/myapp.js
@@ -1,21 +1,43 @@
 'use strict';
 
-function fn() {
-
-}
-
-console.log(typeof fn.prototype === "object");
-
-class MyClass {
-  constructor () {
+class Parent {
+  constructor (...args) {
+    console.log("Parentコンストラクタの処理:", ...args)
   }
   method() {
-    console.log("プロトタイプメソッド");
+    console.log("Parent.prototype.method");
+  }
+  static method() {
+    return "methodです";
   }
 }
 
-console.log(typeof MyClass.prototype.method);
-console.log(MyClass.prototype.constructor === MyClass);
+class Child extends Parent {
+  constructor (...args) {
+    super(...args);
+    console.log("Childコンストラクタの処理:", ...args);
+    console.log(super.constructor);
+  }
+}
 
-const instance = new MyClass();
-instance.method();
+const child = new Child("引数1", "引数2");
+const parent = new Parent("引数1", "引数2");
+child.method();
+
+console.log(Child.method());
+
+console.log(child instanceof Child);
+console.log(parent instanceof Child);
+
+class MyArray extends Array {
+  get first() {
+    return this.at(0);
+  }
+  get last() {
+    return this.at(-1);
+  }
+}
+
+const array = MyArray.from([1,2,3,4,5]);
+console.log(array.first);
+console.log(array.last);
