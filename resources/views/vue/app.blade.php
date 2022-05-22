07ad998c792c6fbf7a92b95354fdaa74070ca35e
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-22 18:41:05 +0900

    [change] v-bind:classに直接オブジェクトを記述。

--- resources/views/vue/app.blade.php
+++ resources/views/vue/app.blade.php
@@ -5,13 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vue.jsの核の機能であるデータバインディング</title>
+    <style>
+        .error {
+            color: #ff0000;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
         <p>@{{ items[0].name }}: @{{ items[0].price}} x @{{ items[0].quantity}}</p>
         <p>小計: @{{ totalPriceWithTax | numberWithDelimiter }}円</p>
         <p>合計(税込): @{{ totalPriceWithTax | numberWithDelimiter }}円</p>
-        <p v-show="!canBuy">
+        <p v-bind:class="{error: !canBuy}">
             @{{ 1000 | numberWithDelimiter }}円以上からご購入いただけます
         </p>
     </div>
