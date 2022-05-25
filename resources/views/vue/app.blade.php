95ed141596473bcff7b14babcb8e03c2da5d82bd
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-25 12:32:17 +0900

    [change] v-bindの省略記法

--- resources/views/vue/app.blade.php
+++ resources/views/vue/app.blade.php
@@ -13,13 +13,14 @@
 </head>
 <body>
     <div id="app">
-        <p v-bind:style="{fontSize: '1.5em'}">買い物</p>
+        <p :style="{fontSize: '1.5em'}">買い物</p>
         <p>@{{ items[0].name }}: @{{ items[0].price}} x @{{ items[0].quantity}}</p>
         <p>小計: @{{ totalPriceWithTax | numberWithDelimiter }}円</p>
         <p>合計(税込): @{{ totalPriceWithTax | numberWithDelimiter }}円</p>
-        <p v-bind:style="errorMessageStyle">
+        <p :class="{error: !canBuy}">
             @{{ 1000 | numberWithDelimiter }}円以上からご購入いただけます
         </p>
+        <button :disabled="canBuy">ボタン</button>
     </div>
     
     <script src="{{ mix('js/app.js') }}"></script>
