528356279736d8c863dc5cbe485f2ac3c3b1fbcf
Masahiro Fujioka <fujioka8700@gmail.com>
2022-05-27 17:41:48 +0900

    [change] Vueコンポーネント

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,71 +31,19 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 // Vueの確認
 console.assert(typeof Vue !== "undefined");
 
-const items = [
-    {
-        name: '鉛筆',
-        price: 300,
-        quantity: 0
-    },
-    {
-        name: 'ノート',
-        price: 400,
-        quantity: 0
-    },
-    {
-        name: '消しゴム',
-        price: 500,
-        quantity: 0
-    },
-];
-
-const vm = new Vue({
-    el: '#app',
-    data: {
-        items: items
-    },
-    filters: {
-        numberWithDelimiter: function(value) {
-            if (!value) {
-                return '0';
-            }
-            return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
-        }
-    },
-    methods: {
-        doBuy: function() {
-            // 本来はここで、サーバーと通信を行う
-            alert(this.totalPriceWithTax + '円のお買い上げ！');
-
-            this.items.forEach(function(item) {
-                item.quantity = 0;
-            });
-        }
-    },
-    computed: {
-        totalPrice: function() {
-            return this.items.reduce((total, item)=>{
-                return total + (item.price * item.quantity);
-            }, 0);
-        },
-        totalPriceWithTax: function() {
-            return Math.floor(this.totalPrice * 1.08);
-        },
-        canBuy: function() {
-            return this.totalPrice >= 1000;
-        },
-        errorMessageClass: function() {
-            return {
-                error: !this.canBuy
-            }
-        },
-        errorMessageStyle: function() {
-            return {
-                border: this.canBuy ? '' : '1px solid red',
-                color:  this.canBuy ? '' : 'red'
-            }
+// コンポーネント
+Vue.component('list-item', {
+    template: '<li>foo {{ contents }}</li>',
+    data: function() {
+        return {
+            contents: 'bar'
         }
     }
+});
+
+// ルート`Vue`インスタンスの作成
+const vm = new Vue({
+    el: '#example',
 });
 
 window.vm = vm;
--- resources/views/vue/app.blade.php
+++ resources/views/vue/app.blade.php
@@ -13,29 +13,9 @@
     </style>
 </head>
 <body>
-    <div id="app">
-        <ul>
-            <li v-for="item in items" v-bind:key="item.name">
-                @{{ item.name }}の個数: <input type="number" v-model="item.quantity" min="0">
-            </li>
-        </ul>
-        <hr>
-        <div v-bind:style="errorMessageStyle">
-            <ul>
-                <li v-for="item in items" v-bind:key="item.name">
-                    @{{ item.name }}: @{{ item.price }} x @{{ item.quantity }} = @{{ item.price * item.quantity | numberWithDelimiter}}円
-                </li>
-            </ul>
-            <p>@{{ items[0].name }}: @{{ items[0].price}} x @{{ items[0].quantity}}</p>
-            <p>小計: @{{ totalPrice | numberWithDelimiter }}円</p>
-            <p>合計(税込): @{{ totalPriceWithTax | numberWithDelimiter }}円</p>
-            <p v-show="!canBuy">
-                @{{ 1000 | numberWithDelimiter }}円以上からご購入いただけます
-            </p>
-            {{-- メソッドが押されたら、メソッドを呼び出す --}}
-            <button v-on:click="doBuy">購入</button>
-        </div>
-    </div>
+    <ul id="example">
+       <list-item></list-item>
+    </ul>
     
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
