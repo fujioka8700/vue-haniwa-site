b069cf608d7397754c7a1848f6b8e740427f2b82
Masahiro Fujioka <fujioka8700@gmail.com>
2022-06-11 05:48:34 +0900

    [change] 子コンポーネントのルート要素におけるスタイルの注意事項

--- resources/js/components/bar.vue
+++ resources/js/components/bar.vue
@@ -1,7 +1,28 @@
 <template>
-    <p class="message">こんにちは</p>
+    <div class="bar">
+        <h1>Barコンポーネント</h1>
+        <p>これはBarコンポーネントです。</p>
+
+		<!-- .headerにマッチする要素が当初想定しないところまで波及する。 -->
+		<!-- こうした影響があるため、コンポーネントに対してclass属性をコンポーネント外から与えるときは注意が必要 -->
+        <foo class="header"/>
+    </div>
 </template>
 
+<script>
+import Foo from './foo'
+export default {
+    components: {
+        Foo
+    }
+}
+</script>
+
 <style scoped>
-    .message { color: #42b983; }
+    .bar {
+        border: 1px solid red;
+        margin: 4px;
+        padding: 4px;
+    }
+    .header { font-size: 125%; }
 </style>
