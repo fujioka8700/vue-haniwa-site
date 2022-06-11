99494deb9766097003b05af226af3588c7da20ba
Masahiro Fujioka <fujioka8700@gmail.com>
2022-06-12 06:26:07 +0900

    [change] Ajaxとactionsオプション

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,31 +16,59 @@ Vue.component('hello-component', HelloComponent);
 // Vueの確認
 console.assert(typeof Vue !== "undefined");
 
+// 例示用に非同期処理を行う関数
+// 実際のアプリではサーバーからデータを取得する
+function getCountNum(type) {
+    return new Promise(resolve => {
+        // 1秒後にtypeに応じたデータを返す
+        setTimeout(() => {
+            let amount;
+            switch (type) {
+                case 'one':
+                    amount = 1;
+                    break;
+                case 'two':
+                    amount = 2;
+                    break;
+                case 'ten':
+                    amount = 10;
+                    break;
+                default:
+                    amount = 0;
+                    break;
+            }
+            resolve({ amount })
+        }, 1000);
+    })
+}
+
 const store = new Vuex.Store({
     state: {
         count: 10
     },
-
     mutations: {
-        increment(state) {
-            state.count += 1;
+        increment(state, payload) {
+            state.count += payload.amount;
         }
     },
-
-    // actionsオプションでアクションを定義する
     actions: {
-        // ステートではなくコンテキストと呼ばれる特別なオブジェクトが渡される
-        incrementAction(ctx) {
-            // incrementミューテーションを実行する
-            ctx.commit('increment');
+        incrementAsync({ commit }, payload) {
+            // 非同期にデータを取得する
+            return getCountNum(payload.type).then(data => {
+                // レスポンスをログに表示
+                console.log(data);
+                
+                // レスポンスをペイロードとして渡したミューテーションを実行する
+                commit('increment', {
+                    amount: data.amount
+                });
+            });            
         }
     }
 });
 
 console.log(store.state.count);
-
-// store.dispatchでアクションを呼び出す
-// incrementActionアクションを呼び出す
-store.dispatch('incrementAction');
-
-console.log(store.state.count);
+store.dispatch('incrementAsync', { type: 'one' }).then(()=>{
+    // アクションの処理が完了した後に実行される
+    console.log(store.state.count);
+});
