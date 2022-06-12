fb8e256916f52b40e910ce1734202dd95c679905
Masahiro Fujioka <fujioka8700@gmail.com>
2022-06-12 09:47:55 +0900

    [change] タスクの一覧表示

--- resources/js/store.js
+++ resources/js/store.js
@@ -3,7 +3,20 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-
+    state: {
+        tasks: [
+            {
+                id: 1,
+                name: '牛乳を買う',
+                done: false
+            },
+            {
+                id: 2,
+                name: 'Vue.jsの本を買う',
+                done: true
+            }
+        ]
+    }
 });
 
 export default store;
