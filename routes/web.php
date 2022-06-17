ebfdb00b89c8c571e7460f4637b5d30c8ea63767
Masahiro Fujioka <fujioka8700@gmail.com>
2022-06-17 15:53:28 +0900

    [clean] Vue.js復習のためファイル整頓

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/javascript', function() {
-    return view('javascript/basic01');
-});
-
 Route::get('/vue', function() {
     return view('vue/app');
-});
-
-Route::get('/altitude', function() {
-    return view('vue/altitude');
-});
-
-Route::get('/sfc', function() {
-    return view('vue/sfc');
-});
-
-Route::get('/vuex', function() {
-    return view('vue/vuex');
-});
-
-Route::get('/tasks', function() {
-    return view('vue/tasks');
-});
-
-Route::get('/store', function() {
-    return view('vue/store');
 });
