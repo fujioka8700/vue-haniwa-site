c2be7ef3d83d8c43e2c992e432322fe620e705cf
Masahiro Fujioka <fujioka8700@gmail.com>
2022-06-20 05:02:49 +0900

    [change] 開発用に sqlite に変更

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/vue', function() {
+Route::get('/{any}', function() {
     return view('vue/app');
-});
+})->where('any', '.*');
